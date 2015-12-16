4993577aff0bd560db2405f5e29e93e9d65bb76e
CMartelLML <corentin.martel@univ-lille1.fr>
2015-12-16 09:49:12 +0100

    fixed version management

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-execfile('./version.py') # read the current version in version.py
+execfile('./crappy/__version__.py') # read the current version in version.py
 
 class LibBuild(build):
   
