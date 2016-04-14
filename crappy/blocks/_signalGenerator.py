2956688da37c17da819913c6160c214a1f986442
CMartelLML <corentin.martel@univ-lille1.fr>
2016-04-14 11:15:46 +0200

    correcting the hold in signalgenerator

--- crappy/blocks/_signalGenerator.py
+++ crappy/blocks/_signalGenerator.py
@@ -291,19 +291,20 @@ The requiered informations depend on the type of waveform you need.
 				elif self.waveform=="hold":
 					#print "holding"
 					while self.time is None or (time.time()-t_step)<self.time:
-						while time.time()-last_t<1./self.send_freq or first:
-							for input_ in self.inputs:
-								if input_.in_.poll() or first: # if there is data waiting
-									recv=input_.recv()
-									df=pd.DataFrame([recv.values()],columns=recv.keys())
-									Data=pd.concat([Data,df],ignore_index=True)
-							first=False
+						while time.time()-last_t<1./self.send_freq:
+							try:
+								for input_ in self.inputs:
+									recv=input_.recv(blocking=False)
+								#first=False
+							except AttributeError: #if no inputs
+								pass
+							#first=False
 							delay(1./(100*1000*self.send_freq))
 						last_t=time.time()
-						last_upper = (Data[self.upper_limit[1]]).last_valid_index()
-						last_lower=(Data[self.lower_limit[1]]).last_valid_index()
-						first_lower=(Data[self.lower_limit[1]]).first_valid_index()
-						first_upper=(Data[self.upper_limit[1]]).first_valid_index()
+						#last_upper = (Data[self.upper_limit[1]]).last_valid_index()
+						#last_lower=(Data[self.lower_limit[1]]).last_valid_index()
+						#first_lower=(Data[self.lower_limit[1]]).first_valid_index()
+						#first_upper=(Data[self.upper_limit[1]]).first_valid_index()
 						if self.step==0:
 							self.alpha=0
 						else:
@@ -313,8 +314,8 @@ The requiered informations depend on the type of waveform you need.
 								self.alpha=0
 							else:
 								pass
-						if last_upper!=first_upper and last_lower!=first_lower: # clean old data
-							Data=Data[min(last_upper,last_lower):]
+						#if last_upper!=first_upper and last_lower!=first_lower: # clean old data
+							#Data=Data[min(last_upper,last_lower):]
 						#Array=pd.DataFrame([[last_t-self.t0,self.alpha,0]],columns=self.labels)
 						Array=OrderedDict(zip(self.labels,[last_t-self.t0,self.alpha,0]))
 						try:
@@ -351,7 +352,7 @@ The requiered informations depend on the type of waveform you need.
 							try:
 								for input_ in self.inputs:
 									recv=input_.recv(blocking=False)
-								first=False
+								#first=False
 							except AttributeError:
 								pass
 							delay(1./(100*1000*self.send_freq))
@@ -395,6 +396,8 @@ The requiered informations depend on the type of waveform you need.
 						#j+=1
 					self.step+=1
 					t_step=time.time()
+					first=True
+					first_of_step=True
 				if self.repeat and self.step==self.nb_step:
 					self.step=0
 			raise Exception("Completed !")
