214a7232a9ba904c9025609d7a67fca7860d20dc
francoisbiar <francois.bari@gmail.com>
2016-07-18 09:20:23 +0200

    REWRITTEN LABJACKSENSOR ADDED thermocouple mode

--- crappy2/sensor/_labJackSensor.py
+++ crappy2/sensor/_labJackSensor.py
@@ -7,6 +7,12 @@
 
 ## @file _labJackSensor.py
 ## @brief  Sensor class for LabJack devices.
+## @author Francois Bari
+## @version 0.2
+## @date 08/07/2016
+
+## @file _labJackSensor.py
+## @brief  Sensor class for LabJack devices.
 ## @author Robin Siemiatkowski
 ## @version 0.1
 ## @date 21/06/2016
@@ -14,179 +20,136 @@
 from labjack import ljm
 import time
 import sys
-from collections import OrderedDict
 from ._meta import acquisition
-from .._warnings import deprecated as deprecated
+import collections
 
 
 class LabJackSensor(acquisition.Acquisition):
-    """Sensor class for LabJack devices."""
+	"""Sensor class for LabJack devices."""
 
-    def __init__(self, channels=[0], chan_range=10, gain=1, offset=0, resolution=0, mode="single", scanRate=100,
-                 scansPerRead=1):
-        """
-        Convert tension value into digital values, on several channels.
+	def __init__(self, mode="single", channels="AIN0", gain=1, offset=0, chan_range=10, resolution=0, handle=None):
+		"""
+		Convert tension value into digital values, on several channels.
+		Available modes at the moment :
 
-        Output is (measured_value * gain) + offset.
+		- Single : Output is (measured_value * gain) + offset.
+		- Thermocouple : Output is a temperature in degree celsius.
 
-        Parameters
-        ----------
-        channel : int/str or list of int/str, default = 0
-        The desired input channel(s). If int, will be assumed as "AIN".
-        chan_range : int or float, default = 10
-        Put the absolute maximum of your expected values.
-        gain : float or list of float, default = 1
-        Multiplication gain for each channel. If there is multiple channels
-        for a single gain, it will be applied to all.
-        offset : float, default = 0
-        Add this value for each channel. If there is multiple channels
-        for a single offset, it will be applied to all.
-        mode : {"single","streamer"}
-        Whether you want to stream value or read value by value. Be aware that the
-        streamer will send values in the LabJack's buffer. Once full, the LabJack
-        will raise an Exception and stop working. You need to keep reading the 
-        buffer in order to keep the streamer working.
-        scanRate : int
-        For "streamer" mode only. Define the reading frequency.
-        scanPerRead : int
-        For "streamer" mode only. Define how many values you want to read at each
-        scans.
-        """
-        super(LabJackSensor, self).__init__()
-        self.channels = list(channels)
-        self.chan_range = chan_range
-        self.gain = gain
-        self.offset = offset
-        self.mode = mode
-        self.scanRate = scanRate
-        self.scansPerRead = scansPerRead
-        self.resolution = resolution
 
-        if type(self.channels) == str or len(self.channels) == 1:  # for get_data
-            self.nchans = 1
-        if type(self.channels) == list:  # if multiple channels
-            self.nchans = len(self.channels)
-            # self.channels=["AIN"+str(chan) for chan in self.channels if type(chan)!=str else chan]
-            self.channels = ["AIN" + str(chan) if type(chan) != str else chan for chan in self.channels]
-            if type(self.gain) == int:
-                self.gain = [self.gain] * self.nchans
-            if type(self.offset) == int:
-                self.offset = [self.offset] * self.nchans
-            if type(self.chan_range) == int or type(self.chan_range) == float:
-                self.chan_range = [self.chan_range] * self.nchans
-            if mode == "single":  # if mode "streamer", the new() must be called in the right process
-                self.new()
-        else:
-            raise Exception("channels must be int or list")
+		Parameters
+		----------
+		channels : int/str or list of int/str, default = 0
+		The desired input channel(s). If int, will be assumed as "AIN".
+		chan_range : int or float, default = 10. Can be 10, 1, 0.1 or 0.01,
+		depending on the range of the voltage to measure. Put the absolute maximum of your expected values.
+		resolution : int, resolution index for each channel (T7 : 0 to 8, T7-PRO : 0 to 12)
+		gain : float, or list of float, default = 1
+		Multiplication gain for each channel. If there is multiple channels
+		for a single gain, it will be applied to all.
+		offset : float, default = 0
+		Add this value for each channel. If there is multiple channels
+		for a single offset, it will be applied to all.
+		"""
+		super(LabJackSensor, self).__init__()
 
-    def new(self):
-        """
-        Initialise the device and create the handle
-        """
-        self.handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-        if self.mode == "streamer":
-            # self.channels = ["AIN0", "AIN1", "AIN2", "AIN3"] #Scan list names to stream
-            numAddresses = len(self.channels)
-            aScanList = ljm.namesToAddresses(numAddresses, self.channels)[0]
-            aName_prefix = ["AIN_ALL_NEGATIVE_CH", "STREAM_SETTLING_US", "STREAM_RESOLUTION_INDEX"]
-            suffixes = ["_RANGE"]
-            aNames = aName_prefix + [chan + s for chan in self.channels for s in suffixes]
-            temp_values = [[self.chan_range[chan]] for chan, a in enumerate(self.channels)]
-            aValues = [ljm.constants.GND, 0, self.resolution] + [item for sublist in temp_values for item in sublist]
-            # print aValues
-            # print "----------------------"
-            # print aNames
-            ljm.eWriteNames(self.handle, len(aNames), aNames, aValues)
+		def var_tester(var, nb_channels):
+			if isinstance(var, (int, float)):
+				var = [var] * nb_channels
+			elif isinstance(var, list) and len(var) == nb_channels and False not in [isinstance(var[i], (int, float))
+			                                                                         for i in range(nb_channels)]:
+				pass
+			else:
+				raise Exception(str(var) + " LabJack error: Wrong parameter definition. "
+				                           "Parameters should be either int, float or a list of such")
+			return var
 
-            # Configure and start stream
-            scanRate = ljm.eStreamStart(self.handle, self.scansPerRead, numAddresses, aScanList, self.scanRate)
+		if not isinstance(channels, list):
+			self.channels = [channels]
+		else:
+			self.channels = channels
 
-        elif self.mode == "single":
-            numAddresses = len(self.channels)
-            # suffixes = ["_EF_INDEX","_NEGATIVE_CH", "_RANGE", "_RESOLUTION_INDEX", "_EF_CONFIG_D", "_EF_CONFIG_E"] # conf for automatic gain/offset
-            suffixes = ["_NEGATIVE_CH", "_RANGE", "_RESOLUTION_INDEX"]
-            aNames = [chan + s for chan in self.channels for s in suffixes]
-            # aValues = [[1,ljm.constants.GND, self.chan_range[chan],0,self.gain[chan],self.offset[chan]] for chan,a in enumerate(self.channels)]
-            aValues = [[ljm.constants.GND, self.chan_range[chan], self.resolution] for chan, _ in
-                       enumerate(self.channels)]
-            aValues = [item for sublist in aValues for item in sublist]  # flatten
-            # print aValues
-            # print "----------------------"
-            # print aNames
-            ljm.eWriteNames(self.handle, len(aNames), aNames, aValues)
+		self.channels = ["AIN" + str(chan) if type(chan) is not str else chan for chan in self.channels]
+		self.nb_channels = len(self.channels)
+		self.channels_index_read = [self.channels[chan] + "_EF_READ_A" for chan in range(self.nb_channels)]
 
-        else:
-            raise Exception("Invalid mode, please select 'single' or 'streamer'")
+		self.gain = var_tester(gain, self.nb_channels)
+		self.offset = var_tester(offset, self.nb_channels)
+		self.chan_range = var_tester(chan_range, self.nb_channels)
+		self.resolution = var_tester(resolution, self.nb_channels)
 
-    def read_stream(self):
-        """
-        Read the signal on all pre-definied channels at the same time.
-        """
-        try:
-            # print "here1"
-            ret = ljm.eStreamRead(self.handle)
-            # print ret
-            data = ret[0]
-            # print "stream1", data
-            try:
-                # print "data : ",data
-                # if self.scansPerRead!=1:
-                # data=zip(*[iter(data)]*self.scansPerRead)
-                curSkip = data.pop(-9999.0)  # delete non-valid values
-                print "skipped some data!"
-            except TypeError:  # No corrupted data
-                data = [a * b + c for a, b, c in
-                        zip(data, self.gain * self.scansPerRead, self.offset * self.scansPerRead)]  # apply coefficients
-                # ret=OrderedDict(zip(['t(s)'],[time.time()]))
-                # ret.update(OrderedDict(zip(self.channels,data)))
-                # results=[a*b+c for a,b,c in zip(results,self.gain,self.offset)]
-                # ret=OrderedDict(zip(['t(s)'],[time.time()]))
-                # ret.update(OrderedDict(zip(channels,results)))
-                # data.insert(0,)
-                # print "data2 : ",data
-                return time.time(), data
-        except ljm.LJMError:
-            # print "error 1"
-            ljme = sys.exc_info()[1]
-            print(ljme)
-            self.close()
-            raise Exception("Error in LabJack Streamer")
-        except Exception:
-            # print "exception in streamer"
-            e = sys.exc_info()[1]
-            print(e)
-            self.close()
-            raise
+		self.mode = mode.lower()
 
-    def get_data(self, mock=None):
-        """
-        Read the signal on all pre-definid channels, one by one.
-        """
-        try:
-            # numFrames = len(channels)
-            # names = ["AIN"+str(chan) if type(chan)!=str else chan for chan in channels]
-            # results = ljm.eReadNames(self.handle, numFrames, names)
-            results = ljm.eReadNames(self.handle, self.nchans, self.channels)
-            results = [a * b + c for a, b, c in zip(results, self.gain, self.offset)]
-            # ret=OrderedDict(zip(['t(s)'],[time.time()]))
-            # ret.update(OrderedDict(zip(channels,results)))
-            # results.insert(0,)
-            return time.time(), results
-        except KeyboardInterrupt:
-            self.close()
-        except Exception:
-            print(sys.exc_info()[1])
-            self.close()
-            raise
+		if not handle:
+			self.handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # open first found labjack
+		else:
+			self.handle = handle
+		self.new()
 
-    def close(self):
-        """
-        Close the device.
-        """
-        try:
-            ljm.eStreamStop(self.handle)
-        except ljm.LJMError:  # if no streamer open
-            pass
-        finally:
-            ljm.close(self.handle)
-            print ("LabJack device closed")
+	def new(self):
+		"""
+		Initialise the device and create the handle
+		"""
+		if ljm.eReadName(self.handle, "WIFI_VERSION") > 0:  # Test if LabJack is a pro or not. Then verifies index values.
+			res_max = 12
+		else:
+			res_max = 8
+
+		assert False not in [0 <= self.resolution[chan] <= res_max for chan in range(self.nb_channels)], \
+			"Wrong definition of resolution index. INDEX_MAX for T7: 8, for T7PRO: 12"
+
+		aValues = []
+		aNames = []
+
+		if self.mode == "single":
+			suffixes = (("_RANGE", self.chan_range),
+			            ("_RESOLUTION_INDEX", self.resolution),
+			            ("_EF_INDEX", 1),
+			            ("_EF_CONFIG_D", self.gain),
+			            ("_EF_CONFIG_E", self.offset))
+
+		elif self.mode == "thermocouple":
+			suffixes = (("_EF_INDEX", 22),  # for thermocouple measures
+			            ("_EF_CONFIG_A", 1), # for degrees C
+			            ("_EF_CONFIG_B", 60052)) # for type K
+
+		else:
+			raise Exception("Unrecognized mode. Check documentation for details.")
+
+		suffixes = collections.OrderedDict(suffixes)
+
+		for chan in range(self.nb_channels):
+			for count, key in enumerate(suffixes):
+				aNames.append(self.channels[chan] + suffixes.keys()[count])
+				if isinstance(suffixes.get(key), list):
+					aValues.append(suffixes.get(key)[chan])
+				else:
+					aValues.append(suffixes.get(key))
+
+		ljm.eWriteNames(self.handle, len(aNames), aNames, aValues)
+
+	def get_data(self, mock=None):
+		"""
+		Read the signal on all pre-defined channels, one by one.
+		"""
+		try:
+			results = ljm.eReadNames(self.handle, self.nb_channels, self.channels_index_read)
+			return time.time(), results
+		except KeyboardInterrupt:
+			# self.close()
+			pass
+		except Exception:
+			print(sys.exc_info()[1])
+			self.close()
+			raise
+
+	def close(self):
+		"""
+		Close the device.
+		"""
+		try:
+			ljm.eStreamStop(self.handle)
+		except ljm.LJMError:  # if no streamer open
+			pass
+		finally:
+			ljm.close(self.handle)
+			print ("LabJack device closed")
