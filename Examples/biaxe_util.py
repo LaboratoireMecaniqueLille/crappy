5487602e232d5c7423a023aebc459e5a75b0be6f
Victor Couty <victor@couty.eu>
2016-07-22 11:24:15 +0200

    Corrected invalid port, added help on biaxe_util.py

--- Examples/biaxe_util.py
+++ Examples/biaxe_util.py
@@ -3,6 +3,24 @@ from __future__ import print_function
 
 import crappy2 as crappy
 
+
+help_string = """
+Syntax:
+{axe}{speed}
+(You can put a space between axe and speed)
+Example: 
+  x -50
+Will pull the axe in at 50% of the speed
+  a20
+Will push out motor A only (the one with numdevice=1)
+
+Use q to quit
+Use clear to clear the errors on all the motors
+Type 'h' or 'help' to see this message again
+"""
+
+
+
 biaxeTech = []
 biaxeTech.append(crappy.technical.Oriental(port='/dev/ttyUSB0'))
 biaxeTech.append(crappy.technical.Oriental(port='/dev/ttyUSB1'))
@@ -19,9 +37,10 @@ for c in 'abcd':
 biaxeDict['x'] = biaxeDict['b']+biaxeDict['d']
 biaxeDict['y'] = biaxeDict['a']+biaxeDict['c']
 
+print(help_string)
 for i in range(4):
-  print("BiaxeTech{0} (on ttyUSB{0}) is motor {1}".format(
-            i,motors[biaxeTech[i].num_device-1]))
+  print("BiaxeTech{} (on {}) is motor {}".format(
+            i,biaxeTech[i].port,motors[biaxeTech[i].num_device-1]))
 
 user_input = ''
 while True:
@@ -29,14 +48,17 @@ while True:
   user_input = raw_input().lower()
   if user_input == 'q':
     break
+  elif user_input in ['h','help','?']:
+    print(help_string)
+    continue
+  elif user_input == 'clear':
+    for axe in biaxeTech:
+      axe.clear_errors()
+    continue
   try:
     axe = user_input[:1]
     speed = int(user_input[1:].strip())
   except ValueError:
-    if user_input == 'clear':
-      for axe in biaxeTech:
-        axe.clear_errors()
-      continue
     print("Unknow command, stopping")
     for axe in biaxeTech:
       axe.actuator.set_speed(0)
