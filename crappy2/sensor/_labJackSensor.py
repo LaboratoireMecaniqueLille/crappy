880c794582fe35884632d3806ce1d717ad8bbde0
francoisbiar <francois.bari@gmail.com>
2016-08-18 13:12:16 +0200

    NEW : labjacksensor lots of improvements

--- crappy2/sensor/_labJackSensor.py
+++ crappy2/sensor/_labJackSensor.py
@@ -8,47 +8,67 @@
 ## @file _labJackSensor.py
 # @brief  Sensor class for LabJack devices.
 # @author Francois Bari
-# @version 0.2
-# @date 08/07/2016
+# @version 0.9
+# @date 18/08/2016
 
 from labjack import ljm
-import time
-import sys
+from time import time
+from sys import exc_info
 from ._meta import acquisition
 from collections import OrderedDict
-import Tkinter
+from multiprocessing import Process, Queue
+from Tkinter import Tk, Label
 
 
 class LabJackSensor(acquisition.Acquisition):
     """Sensor class for LabJack devices."""
 
     def __init__(self, mode="single", channels="AIN0", gain=1, offset=0, chan_range=10, resolution=0, scan_rate=100,
-                 handle=None, identifier=None):
+                 scans_per_read=None, handle=None, identifier=None):
         """
-        Convert tension value into digital values, on several channels.
-
-        Available modes at the moment :
-
-        - Single : Output is (measured_value * gain) + offset, can acquire at 1 kHz max.
-        - Thermocouple : Output is a temperature in degree celsius.
-        - Streamer : Output is (measured_value * gain) + offset, can acquire at 100 kHz max.
+        Convert tension value into digital values, on several channels, using LabJack Devices.
 
         Args:
-            channels : int/str or list of int/str, default = 0
-                        The desired input channel(s). If int, will be assumed as "AIN".
-            chan_range : int or float, default = 10. Can be 10, 1, 0.1 or 0.01,
-                        depending on the range of the voltage to measure. Put the absolute maximum of your expected values.
-            resolution : int, resolution index for each channel (T7 : 0 to 8, T7-PRO : 0 to 12)
-            gain : float, or list of float, default = 1
-                    Multiplication gain for each channel. If there is multiple channels
-                    for a single gain, it will be applied to all.
-            offset : float, default = 0
-                    Add this value for each channel. If there is multiple channels
-                    for a single offset, it will be applied to all.
+
+            mode :         str.
+                           Available modes at the moment :
+                           - Single : Output is (measured_value * gain) + offset, can acquire at 1 kHz max.
+                           - Thermocouple : Output is a temperature in degree celsius.
+                           - Streamer : Output is measured_value, can acquire at 100 kHz max.
+
+            channels :     int, str or list of int or str, default = 0
+                           The desired input channel(s). If int, will be assumed as "AIN".
+
+            gain :         float or list of float, default = 1
+                           Multiplication gain for each channel. If there is multiple channels
+                           for a single gain, it will be applied to all.
+
+            offset :       float, default = 0
+                           Add this value for each channel. If there is multiple channels
+                           for a single offset, it will be applied to all.
+
+            chan_range :   int or float, default = 10. Can be 10, 1, 0.1 or 0.01, depending on the voltage range
+                           to measure. Put the absolute maximum of your expected values.
+
+            resolution :   int, resolution index for each channel (T7 : 0 to 8, T7-PRO : 0 to 12) ~11 to 22 bits
+                           depending on the device, the chan_range and the resolution index.
+                           higher resolution index = higher resolution, but higher latency.
+
+            scan_rate :    STREAMER MODE ONLY : int, defines how many scans to perform during streaming.
+                           Effective acquisition frequency per channel = scan_rate/nb_channels
+
+            scans_per_read:STREAMER MODE ONLY : int, defines how many samples to collect during one loop.
+                           If undefined, will be assumed as a fraction of scan_rate, determined for performance.
+
+            handle:        Used if using labjack as I/O device at the same time. Unused for the moment (18/08/2016)
+
+            identifier:    str. Used if multiple labjacks are connected. The identifier could be anything
+                           that could define the device : serial number, name, wifi version..
             """
         super(LabJackSensor, self).__init__()
 
         def var_tester(var, nb_channels):
+            """Used to check if the user entered correct parameters."""
             if isinstance(var, (int, float)):
                 var = [var] * nb_channels
             elif isinstance(var, list) and len(var) == nb_channels and False not in [isinstance(var[i], (int, float))
@@ -77,97 +97,146 @@ class LabJackSensor(acquisition.Acquisition):
         self.mode = mode.lower()
 
         if self.mode == "streamer":
+            """Additionnal variables for streamer function only."""
             self.a_scan_list = ljm.namesToAddresses(self.nb_channels, self.channels)[0]
             self.scan_rate = scan_rate
-            self.scans_per_read = int(scan_rate / 10.)
-
+            self.scans_per_read = scans_per_read if not None else int(self.scan_rate / 5.)
+            global queue
+            queue = Queue()
         self.handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, str(identifier) if identifier else "ANY") \
-            if not handle else handle  # open first found labjack OR labjack identified
+            if not handle else handle  # open first found labjack OR identified labjack
 
         self.new()
 
+    class DialogBox:
+        """Dialog box that pops when using streamer function."""
+        def __init__(self, scan_rate, scans_per_sample):
+            self.root = Tk()
+            self.root.title('LabJack Streamer Information')
+            self.root.resizable(width=False, height=False)
+
+            self.first_column = \
+                ['Set Scan Rate', 'Samples Collecting Rate', 'Chronometer', 'Device Buffer', 'Software Buffer']
+            self.second_column = \
+                ['%.1f kHz' % (scan_rate / 1000), '%.1f kScans' % (scans_per_sample / 1000), 0.0, 0, 0]
+            for row_index, first_column in enumerate(self.first_column):
+                Label(self.root, text=first_column, borderwidth=10).grid(row=row_index, column=0)
+                Label(self.root, text=self.second_column[row_index], borderwidth=10).grid(row=row_index, column=1)
+            self.update()
+
+        def update(self):
+            """Method to update data inside the dialog box. The window is updated every time data in queue occurs."""
+            try:
+                global queue
+                array = queue.get()
+                t0 = array[0]
+                while True:
+                    array[0] = '%.1f' % (array[0] - t0)
+                    for row_index, value in enumerate(array):
+                        Label(self.root, text=value, borderwidth=10).grid(row=row_index + 2, column=1)
+                    self.root.update()
+                    array = queue.get()
+            except KeyboardInterrupt:
+                pass
+            except Exception:
+                raise
+
     def new(self):
         """
-        Initialise the device and create the handle
-        """
-        res_max = 12 if ljm.eReadName(self.handle, "WIFI_VERSION") > 0 else 8  # Test if LabJack is pro or not
-        assert False not in [0 <= self.resolution[chan] <= res_max for chan in range(self.nb_channels)], \
-            "Wrong definition of resolution index. INDEX_MAX for T7: 8, for T7PRO: 12"
-
-        if self.mode == "single":
-
-            suffixes = OrderedDict(("_RANGE", self.chan_range),
-                                   ("_RESOLUTION_INDEX", self.resolution),
-                                   ("_EF_INDEX", 1),                        # for applying a slope and offset
-                                   ("_EF_CONFIG_D", self.gain),             # index to set the gain
-                                   ("_EF_CONFIG_E", self.offset))           # index to set the offset
-
-        elif self.mode == "thermocouple":
-            suffixes = OrderedDict(("_EF_INDEX", 22),                       # for thermocouple measures
-                                   ("_EF_CONFIG_A", 1),                     # for degrees C
-                                   ("_EF_CONFIG_B", 60052))                 # for type K
-
-        elif self.mode == "streamer":
-            a_names = ["AIN_ALL_RANGE", "STREAM_SETTLING_US", "STREAM_RESOLUTION_INDEX"]
-            a_values = [int(self.chan_range[0]), 0, int(self.resolution[0])]
-      #     a_names = ["AIN_ALL_NEGATIVE_CH", "AIN_ALL_RANGE", "STREAM_SETTLING_US",
-      #                 "STREAM_RESOLUTION_INDEX"]
-      #     a_values = [ljm.constants.GND, self.chan_range, 0, 0]
-
-        else:
-            raise Exception("Unrecognized mode. Check documentation.")
-
-        if self.mode == "single" or self.mode == "thermocouple":
-            a_values = []
-            a_names = []
-            for chan_iteration in range(self.nb_channels):
-                for count, key in enumerate(suffixes):
-                    a_names.append(self.channels[chan_iteration] + suffixes.keys()[count])
-                    if isinstance(suffixes.get(key), list):
-                        a_values.append(suffixes.get(key)[chan_iteration])
-                    else:
-                        a_values.append(suffixes.get(key))
-        ljm.eWriteNames(self.handle, len(a_names), a_names, a_values)
-
-    def start_stream(self):
-        # self.root = Tkinter.Tk()
-        # self.rows = ['Chronometer', 'Device Buffer', 'Software Buffer']
-        #
-        # self.root.title('LabJack Streamer')
-        #
-        # for row_index, row in enumerate(self.rows):
-        #     Tkinter.Label(self.root, text=row, borderwidth=10).grid(row=row_index, column=0)
-        #
-        # self.root.resizable(width=False, height=False)
-        # self.root.mainloop()
-        ljm.eStreamStart(self.handle, self.scans_per_read, self.nb_channels, self.a_scan_list, self.scan_rate)
-
-    def get_data(self, mock=None):
-        """
-        Read the signal on all pre-defined channels, one by one.
-            \todo
-              Make the acquisition more efficient. Add an option to stream more than 1 channel.
+        Initialize the device.
         """
         try:
-            if not self.mode == "streamer":
-                results = ljm.eReadNames(self.handle, self.nb_channels, self.channels_index_read)
-                return time.time(), results
+            res_max = 12 if ljm.eReadName(self.handle, "WIFI_VERSION") > 0 else 8  # Test if LabJack is pro or not
+            assert False not in [0 <= self.resolution[chan] <= res_max for chan in range(self.nb_channels)], \
+                "Wrong definition of resolution index. INDEX_MAX for T7: 8, for T7PRO: 12"
+
+            if self.mode == "single":
+                to_write = OrderedDict([
+                                       ("_RANGE", self.chan_range),
+                                       ("_RESOLUTION_INDEX", self.resolution),
+                                       ("_EF_INDEX", 1),                        # for applying a slope and offset
+                                       ("_EF_CONFIG_D", self.gain),             # index to set the gain
+                                       ("_EF_CONFIG_E", self.offset)            # index to set the offset
+                                       ])
+
+            elif self.mode == "thermocouple":
+                to_write = OrderedDict([
+                                       ("_EF_INDEX", 22),                       # for thermocouple measures
+                                       ("_EF_CONFIG_A", 1),                     # for degrees C
+                                       ("_EF_CONFIG_B", 60052)                  # for type K
+                                       ])
+
+            elif self.mode == "streamer":
+                a_names = ["AIN_ALL_RANGE", "STREAM_SETTLING_US", "STREAM_RESOLUTION_INDEX"]
+                a_values = [int(self.chan_range[0]), 0, int(self.resolution[0])]
+
             else:
-                retrieved_from_buffer = ljm.eStreamRead(self.handle)
-                results = retrieved_from_buffer[0]
-                timer = time.time()
-                # Tkinter.Label(self.root, text=timer, borderwidth=10).grid(row=0, column=1)
-                # Tkinter.Label(self.root, text=retrieved_from_buffer[1], borderwidth=10).grid(row=1, column=1)
-                # Tkinter.Label(self.root, text=retrieved_from_buffer[2], borderwidth=10).grid(row=2, column=1)
-                # self.root.update()
-                print "Left on Device Buffer: %i " % retrieved_from_buffer[1]
-                print "Left on LJM Buffer: %i " % retrieved_from_buffer[2]
-                return timer, results
+                raise Exception("Unrecognized mode. Check documentation.")
+
+            if self.mode == "single" or self.mode == "thermocouple":
+                a_values = []
+                a_names = []
+                for chan_iteration in range(self.nb_channels):
+                    for count, key in enumerate(to_write):
+                        a_names.append(self.channels[chan_iteration] + to_write.keys()[count])
+                        if isinstance(to_write.get(key), list):
+                            a_values.append(to_write.get(key)[chan_iteration])
+                        else:
+                            a_values.append(to_write.get(key))
+            ljm.eWriteNames(self.handle, len(a_names), a_names, a_values)
+        except:
+            self.close()
+            raise
+
+    def start_stream(self):
+        """Method to initialize a streaming data."""
+        try:
+            ljm.eStreamStart(self.handle, self.scans_per_read, self.nb_channels, self.a_scan_list, self.scan_rate)
+            Process(target=self.DialogBox, args=(self.scan_rate, self.scans_per_read)).start()
+
         except KeyboardInterrupt:
             self.close()
             pass
         except Exception:
-            print(sys.exc_info()[1])
+            raise
+
+    def get_data(self, mock=None):
+        """
+        Read the signal on all pre-defined channels, one by one.
+        """
+        try:
+            results = ljm.eReadNames(self.handle, self.nb_channels, self.channels_index_read)
+            return time(), results
+
+        except KeyboardInterrupt:
+            self.close()
+            pass
+
+        except Exception:
+            print(exc_info()[1])
+            self.close()
+            raise
+
+    def get_stream(self):
+        """
+        Read the device buffer if scan_mode is set.
+        """
+        global queue
+        try:
+            retrieved_from_buffer = ljm.eStreamRead(self.handle)
+            results = retrieved_from_buffer[0]
+            timer = time()
+            queue.put([timer, retrieved_from_buffer[1], retrieved_from_buffer[2]])
+            return timer, results
+
+        except KeyboardInterrupt:
+            ljm.eStreamStop(self.handle)
+            self.close()
+            pass
+
+        except Exception:
+            print(exc_info()[1])
+            ljm.eStreamStop(self.handle)
             self.close()
             raise
 
@@ -175,10 +244,14 @@ class LabJackSensor(acquisition.Acquisition):
         """
         Close the device.
         """
-        try:
+        if ljm.eReadName(self.handle, "STREAM_ENABLE"):
             ljm.eStreamStop(self.handle)
-        except ljm.LJMError:  # if no streamer open
-            pass
-        finally:
-            ljm.close(self.handle)
-            print ("LabJack device closed")
+        ljm.close(self.handle)
+        print "LabJack device closed"
+        # try:
+        #     ljm.eStreamStop(self.handle)
+        # except ljm.LJMError:  # if no streamer open
+        #     pass
+        # finally:
+        #     ljm.close(self.handle)
+        #     print ("LabJack device closed")
