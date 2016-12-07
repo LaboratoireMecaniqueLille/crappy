c0798572c00820e8d42e5f1e34a50ca740a9be66
Victor Couty <victor@couty.eu>
2016-12-07 13:30:12 +0100

    Added basic.py, the most minimalist example

---
+++ Examples/basic.py
@@ -0,0 +1,21 @@
+﻿#coding: utf-8
+
+import crappy
+
+if __name__ == "__main__":
+
+	print "Creating fake camera..."
+	c = crappy.blocks.FakeCamera(512,512)
+	print "Creating sink..."
+	s = crappy.blocks.Sink()
+
+	print "Creating link..."
+	l = crappy.links.Link()
+
+	print "Linking..."
+	c.add_output(l)
+	s.add_input(l)
+
+	print "Starting..."
+	crappy.start()
+	print "Started !"
