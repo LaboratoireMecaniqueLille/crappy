df380c447bae93427d4e70aa9b1af68d0da8f815
Victor Couty <victor@couty.eu>
2016-12-07 11:08:15 +0100

    Added crappy.start to automatically start the blocks

--- crappy/__init__.py
+++ crappy/__init__.py
@@ -1,5 +1,7 @@
 # coding: utf-8
+from __future__ import print_function
 
+import time
 from . import sensor
 from . import actuator
 from . import technical
@@ -7,3 +9,22 @@ from . import blocks
 from . import links
 from ._warnings import deprecated, import_error
 from .__version__ import __version__
+
+def start(verbose=True):
+  if verbose:
+    def vprint(*args):
+      print("[crappy.start]",*args)
+  else:
+    vprint = lambda x: None
+  t0=time.time()
+  vprint("Setting t0 to",time.strftime("%d %b %Y, %H:%M:%S"
+                                      ,time.localtime(t0)))
+  for instance in blocks._meta.MasterBlock.instances:
+    instance.t0 = t0
+
+  vprint("Starting the blocks...")
+  for instance in blocks._meta.MasterBlock.instances:
+    vprint("Starting",instance)
+    instance.start()
+    vprint("Started, PID:",instance.pid)
+  vprint("All blocks are started.")
