022a847182e80861d6719e4d514358020938de0f
Victor Couty <victor@couty.eu>
2016-12-12 14:52:02 +0100

    Updated displayre example to show new syntax

--- Examples/displayer.py
+++ Examples/displayer.py
@@ -7,23 +7,23 @@ import time
 Very simple program that displays the output of a camera
 """
 if __name__ == "__main__":
-	camera = crappy.blocks.StreamerCamera("Ximea", numdevice=0, freq=20, save=False,save_directory="CHANGEME",xoffset=0, yoffset=0, width=2048, height=2048)
-	#camera = crappy.blocks.FakeCamera(512,512)
+  #camera = crappy.blocks.StreamerCamera("Ximea", numdevice=0, freq=20, save=False,save_directory="CHANGEME",xoffset=0, yoffset=0, width=2048, height=2048)
+#camera = crappy.blocks.FakeCamera(512,512)
+  camera = crappy.blocks.StreamerCamera("Webcam")
 
-	disp = crappy.blocks.CameraDisplayer(framerate=10)
+  disp = crappy.blocks.CameraDisplayer(framerate=10)
 
-	link = crappy.links.Link()
+#link = crappy.links.Link()
+  crappy.links.Link(camera,disp)
 
-	camera.add_output(link)
-	disp.add_input(link)
+#	camera.add_output(link)
+#	disp.add_input(link)
 
-	t0=time.time()
-	for instance in crappy.blocks._meta.MasterBlock.instances:
-	  instance.t0 = t0
+#	t0=time.time()
+#	for instance in crappy.blocks._meta.MasterBlock.instances:
+#	  instance.t0 = t0
+#
+#	for instance in crappy.blocks._meta.MasterBlock.instances:
+#	  instance.start()
 
-	for instance in crappy.blocks._meta.MasterBlock.instances:
-	  instance.start()
-
-	while True:
-	  time.sleep(1)
-	  print "Elapsed:",time.time()-t0
+  crappy.start()
