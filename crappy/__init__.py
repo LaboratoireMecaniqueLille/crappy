fb8f006f04331505836cb00f9f80a1e26f32f628
Victor Couty <victor@couty.eu>
2016-12-14 13:09:00 +0100

    Moved crappy.start to a separate file

--- crappy/__init__.py
+++ crappy/__init__.py
@@ -1,7 +1,5 @@
 # coding: utf-8
-from __future__ import print_function
 
-import time
 from . import sensor
 from . import actuator
 from . import technical
@@ -9,22 +7,4 @@ from . import blocks
 from . import links
 from ._warnings import deprecated, import_error
 from .__version__ import __version__
-
-def start(verbose=True):
-  if verbose:
-    def vprint(*args):
-      print("[crappy.start]",*args)
-  else:
-    vprint = lambda x: None
-  t0=time.time()
-  vprint("Setting t0 to",time.strftime("%d %b %Y, %H:%M:%S"
-                                      ,time.localtime(t0)))
-  for instance in blocks._meta.MasterBlock.instances:
-    instance.t0 = t0
-
-  vprint("Starting the blocks...")
-  for instance in blocks._meta.MasterBlock.instances:
-    vprint("Starting",instance)
-    instance.start()
-    vprint("Started, PID:",instance.pid)
-  vprint("All blocks are started.")
+from _start import start
---
+++ crappy/_start.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+from __future__ import print_function
+
+import time
+from blocks._meta import MasterBlock
+
+def start(verbose=True):
+  if verbose:
+    def vprint(*args):
+      print("[crappy.start]",*args)
+  else:
+    vprint = lambda *x: None
+  t0=time.time()
+  vprint("Setting t0 to",time.strftime("%d %b %Y, %H:%M:%S"
+                                      ,time.localtime(t0)))
+  for instance in MasterBlock.instances:
+    instance.t0 = t0
+
+  vprint("Starting the blocks...")
+  try:
+    for instance in MasterBlock.instances:
+      vprint("Starting",instance)
+      instance.start()
+      vprint("Started, PID:",instance.pid)
+    vprint("All blocks are started.")
+  except (Exception, KeyboardInterrupt) as e:
+    print("Exception in main :", e)
+    for instance in crappy.blocks.MasterBlock.instances:
+      try:
+        instance.stop()
+      except Exception as e:
+        print(e)
