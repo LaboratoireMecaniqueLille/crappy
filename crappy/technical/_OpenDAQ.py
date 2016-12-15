c28a2832a6a0eb39809a3bc3a5ecef9407016569
francoisbiar <francois.bari@gmail.com>
2016-12-15 17:40:16 +0100

    can now read 8 channels

--- crappy/technical/_OpenDAQ.py
+++ crappy/technical/_OpenDAQ.py
@@ -1,33 +1,42 @@
 from opendaq import DAQ
 from time import time
 from ._meta import io
-
+from operator import itemgetter
 
 class OpenDAQ(io.Control_Command):
-    """
-    Class for openDAQ Devices.
-    """
+  """
+  Class for openDAQ Devices.
+  """
 
-    def __init__(self, *args, **kwargs):
-        self.input_channels = kwargs.get('channels', 1)  # Possible values: 1..8
-        self.input_gain = kwargs.get('gain', 1)  # Possible values: 0..4 (x1/3, x1, x2, x10, x100)
-        self.input_offset = kwargs.get('offset', 0)  # not a parameter. apply after reading it.
-        self.input_nsamples_per_read = kwargs.get('nsamples', 20)  # possible values : 0..254
-        self.mode = kwargs.get('mode', 'single')
-        self.new()
+  def __init__(self, *args, **kwargs):
 
-    def new(self):
-        self.handle = DAQ("/dev/ttyUSB0")
-        self.handle.conf_adc(pinput=self.input_channels, ninput=0, gain=self.input_gain, nsamples=self.input_nsamples_per_read)
-        pass
+    self.input_channels = kwargs.get('channels', 1)  # Possible values: 1..8
+    self.nchannels = 1 if not isinstance(self.input_channels, list) else len(self.input_channels)
+    self.input_gain = kwargs.get('gain', 0)  # Possible values: 0..4 (x1/3, x1, x2, x10, x100)
+    self.input_offset = kwargs.get('offset', 0)  # not a parameter. apply after reading it.
+    self.input_nsamples_per_read = kwargs.get('nsamples', 20)  # possible values : 0..254
+    self.mode = kwargs.get('mode', 'single')
+    self.i = 0
+    self.new()
+    if self.nchannels > 1:
+      self.getter = itemgetter(*self.input_channels)
+  def new(self):
+    self.handle = DAQ("/dev/ttyUSB0")
+    if self.nchannels == 1:
+      self.handle.conf_adc(pinput=self.input_channels, ninput=0, gain=self.input_gain,
+                           nsamples=self.input_nsamples_per_read)
 
-    def get_data(self, mock=None):
-        data = self.handle.read_analog()
-        return time(), [data]
+  def get_data(self, mock=None):
+    if self.nchannels == 1:
+      data = [self.handle.read_analog()]
+    else:
+      data = list(self.getter(self.handle.read_all(self.input_nsamples_per_read)))
+    self.i += 1
+    return time(), data
 
-    def set_cmd(self, command):
-        self.handle.set_dac(command)
+  def set_cmd(self, command):
+    self.handle.set_dac(command)
 
-    def close(self):
-        self.handle.close()
-        pass
+  def close(self):
+    self.handle.close()
+    pass
--- tests/test_opendaq.py
+++ tests/test_opendaq.py
@@ -3,7 +3,7 @@ import numpy as np
 from time import sleep, time
 
 #
-opendaq = crappy.technical.OpenDAQ(channels=1, nsamples=100, gain=0)
+opendaq = crappy.technical.OpenDAQ(channels=[1, 2, 5, 6], nsamples=20, gain=0)
 # labjack = crappy.technical.LabJack(actuator={'channel': 'TDAC0'},
 #                                    sensor={'channels': 'AIN0', 'resolution': 8, 'range_num': 10})
 #
@@ -12,62 +12,64 @@ opendaq = crappy.technical.OpenDAQ(channels=1, nsamples=100, gain=0)
 # grapher_labjack = crappy.blocks.Grapher(('t(s)', 'AIN0'), length=10)
 # crappy.link(measure_labjack, compact_labjack, name='pou')
 # crappy.link(compact_labjack, grapher_labjack, name='dav')
-
-measure_opendaq = crappy.blocks.MeasureByStep(opendaq, labels=['t(s)', 'AN1'])
-grapher_opendaq = crappy.blocks.Grapher(('t(s)', 'AN1'), length=100)
+labels = ['t(s)', 'AN1', 'AN2', 'AN5', 'AN6']
+measure_opendaq = crappy.blocks.MeasureByStep(opendaq, labels=labels)
+grapher_opendaq = crappy.blocks.Grapher([('t(s)', label) for label in labels[1:]], length=100)
+saver_opendaq = crappy.blocks.Saver('/home/francois/A_Projects/015_test_opendaq/test.csv', stamp='rioye')
 compact_opendaq = crappy.blocks.Compacter(100)
 crappy.link(measure_opendaq, compact_opendaq, name='toto')
 crappy.link(compact_opendaq, grapher_opendaq, name='foo')
-
+crappy.link(compact_opendaq, saver_opendaq)
+#
 crappy.start()
+#
+# volt = 0
+# x = np.linspace(0, 2 * np.pi, 100)
+# sine = np.sin(x)
+# period = 0
+# # damping = 0.
+# labjack_actuator = 0.
+# t0 = 0
+# while True:
+#   try:
+#     if labjack_actuator:
+#       for i in xrange(len(sine)):lmsfkjkdlbjflkuj
+#         volt = sine[i] * damping
+#         labjack.set_cmd(volt)
+#         opendaq.set_cmd(volt)
+#         sleep(1)
+#         damping = damping + 0.001 if damping < 1 else 1
+#         pass
+#         period += 1
+#     else:
+#       volt = (sine * 2047 + 2048).round()
+#       for i in xrange(len(sine)):
+#         print 'valeur actuelle:', volt[i]
+#         opendaq.set_cmd(volt[i])
+#
+#         sleep(0.0005)
+#       period += 1
+#       # print 'frequence:', time() - t0
+#   except Exception as e:
+#     print 'nombre de periodes:', period
+#     print e
+#     raise
 
-volt = 0
-x = np.linspace(0, 2 * np.pi, 100)
-sine = np.sin(x)
-period = 0
-# damping = 0.
-labjack_actuator = 0.
-t0 = 0
-while True:
-  try:
-    if labjack_actuator:
-      for i in xrange(len(sine)):lmsfkjkdlbjflkuj
-        volt = sine[i] * damping
-        labjack.set_cmd(volt)
-        opendaq.set_cmd(volt)
-        sleep(1)
-        damping = damping + 0.001 if damping < 1 else 1
-        pass
-        period += 1
-    else:
-      volt = (sine * 2047 + 2048).round()
-      for i in xrange(len(sine)):
-        print 'valeur actuelle:', volt[i]
-        opendaq.set_cmd(volt[i])
-
-        sleep(0.0005)
-      period += 1
-      # print 'frequence:', time() - t0
-  except Exception as e:
-    print 'nombre de periodes:', period
-    print e
-    raise
-
-  import time
-
-  # from opendaq import DAQ
-  # dq = DAQ("/dev/ttyUSB0")
-  # liste = [1, 2, 3]
-  # dq.conf_adc(pinput=1, ninput=0, gain=2, nsamples=20)
-  # i = 0.
-  # while True:
-  #     try:
-  #         x = i/1000 % 3
-  #         data = dq.read_analog()
-  #         print data
-  #         i += 1
-  #         # time.sleep(0.1)
-  #     except:
-  #         dq.close()
-  #         break
-  # # gain et offset : 1004, -12
+# import time
+#
+# from opendaq import DAQ
+# dq = DAQ("/dev/ttyUSB0")
+# dq.conf_adc(pinput=1, ninput=0, gain=2, nsamples=1)
+# i = 0.
+# t0 = time.time()
+# while True:
+#   try:
+#     data = dq.read_all(nsamples=20)
+#     i += 1
+#     if time.time() - t0 > 10:
+#       tfinal = time.time()
+#       print 'frequence:', i / (tfinal - t0)
+#       break
+#   except:
+#     dq.close()
+#     break
