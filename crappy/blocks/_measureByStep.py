e9315f946716238f1de2a1659c4df86611a2d6ed
francoisbiar <francois.bari@gmail.com>
2017-01-05 09:49:58 +0100

    added verbosity, refactored code

--- crappy/blocks/_measureByStep.py
+++ crappy/blocks/_measureByStep.py
@@ -12,11 +12,14 @@
 # @version 0.1
 # @date 11/07/2016
 
+from __future__ import print_function
 from _masterblock import MasterBlock
 import time
-import os
 from collections import OrderedDict
 from ..links._link import TimeoutError
+import threading
+from Queue import Queue
+import sys
 
 
 class MeasureByStep(MasterBlock):
@@ -24,7 +27,7 @@ class MeasureByStep(MasterBlock):
   Streams value measured on a card through a Link object.
   """
 
-  def __init__(self, sensor, labels=None, freq=None):
+  def __init__(self, sensor, *args, **kwargs):
     """
     This streamer read the value on all channels ONE BY ONE and send the values through a Link object.
 
@@ -45,8 +48,38 @@ class MeasureByStep(MasterBlock):
     """
     super(MeasureByStep, self).__init__()
     self.sensor = sensor
-    self.labels = labels if labels else ["time(sec)"] + self.sensor.channels
-    self.freq = freq
+    assert sensor, 'ERROR in MeasureByStep: no sensor defined.'
+    self.labels = kwargs.get('labels', ["time(sec)"] + [self.sensor.channels])
+    self.freq = kwargs.get('freq', None)
+    self.verbose = kwargs.get('verbose', None)
+    if self.verbose:
+      self.nb_acquisitions = 0.
+      global queue, last_len
+      last_len = None
+      queue = Queue()
+
+  def print_time(self):
+    def reprint(*args):
+      global last_len
+      s = " ".join([str(i) for i in args])
+      s = s.split("\n")[0]
+      l = len(s)
+      if last_len is not None:
+        s += " " * (last_len - l)
+        sys.stdout.write("\033[F")
+      last_len = l
+      print(s)
+
+    nb_points0 = 0.
+    while True:
+      nb_points1 = queue.get()
+      reprint('Freq:', nb_points1 - nb_points0, 'Hz')
+      nb_points0 = nb_points1
+
+  def temporization(self, timer):
+    while time.time() - timer < 1. / self.freq:
+      time.sleep(1. / (100 * self.freq))
+    pass
 
   def main(self):
     """
@@ -55,13 +88,15 @@ class MeasureByStep(MasterBlock):
     """
     try:
       trigger = "internal" if len(self.inputs) == 0 else "external"
-      timer = time.time()
+      if self.verbose:
+        printer = threading.Thread(target=self.print_time)
+        printer.daemon = True
+        printer.start()
+        elapsed = 5.
       while True:
         if trigger == "internal":
           if self.freq:  # timing loop
-            while time.time() - timer < 1. / self.freq:
-              time.sleep(1. / (100 * self.freq))
-            timer = time.time()
+            self.temporization(time.time())
           sensor_epoch, sensor_values = self.sensor.get_data("all")
           chronometer = sensor_epoch - self.t0
           sensor_values.insert(0, chronometer)
@@ -76,11 +111,17 @@ class MeasureByStep(MasterBlock):
         try:
           for output in self.outputs:
             output.send(results)
+          if self.verbose:
+            self.nb_acquisitions += 1
+
+            if chronometer - elapsed > 1.:
+              queue.put(self.nb_acquisitions)
+              elapsed = chronometer
         except TimeoutError:
           raise
         except AttributeError:  # if no outputs
           pass
 
     except (Exception, KeyboardInterrupt) as e:
-      print "Exception in measureByStep : ", e
+      print("Exception in measureByStep :", e)
       self.sensor.close()
