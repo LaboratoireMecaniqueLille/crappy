9c5fb6e84a5eddcbfcbfdf396b57e36b2bcc40d6
francoisbiar <francois.bari@gmail.com>
2017-01-17 10:08:57 +0100

    [start, compacterblock] formated with PEP8 convention

--- crappy/_start.py
+++ crappy/_start.py
@@ -4,7 +4,8 @@ from __future__ import print_function
 import time
 from blocks import MasterBlock
 
-def launch(t0=None,verbose=True):
+
+def launch(t0=None, verbose=True):
   if verbose:
     def vprint(*args):
       print("[crappy.launch]", *args)
@@ -17,9 +18,10 @@ def launch(t0=None,verbose=True):
   for instance in MasterBlock.instances:
     instance.launch(t0)
   t1 = time.time()
-  vprint("All blocks loop started. It took",(t1-t0)*1000,"ms")
+  vprint("All blocks loop started. It took", (t1 - t0) * 1000, "ms")
 
-def start(verbose=True,start_loop=True):
+
+def start(verbose=True, start_loop=True):
   if verbose:
     def vprint(*args):
       print("[crappy.start]", *args)
@@ -44,7 +46,7 @@ def start(verbose=True,start_loop=True):
   if not start_loop:
     return
   vprint("Waiting for all blocks to be ready...")
-  while not all(map(lambda x:x.status == "ready",MasterBlock.instances)):
+  while not all(map(lambda x: x.status == "ready", MasterBlock.instances)):
     time.sleep(.1)
   vprint("All blocks ready, let's go !")
   launch(verbose=verbose)
--- crappy/blocks/_compacterblock.py
+++ crappy/blocks/_compacterblock.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 ##  @addtogroup blocks
 # @{
 
@@ -18,13 +18,15 @@ from collections import OrderedDict
 
 import time
 
+
 class CompacterBlock(MasterBlock):
   """
   A class to represent any block that will need to stream data by chunks
   Usually used for data acquisition at any frequency (unless it is under 1~10Hz)
   """
-  def __init__(self,labels,compacter):
-    print("[CompacterBlock] Got labels", labels,"comp=",compacter)
+
+  def __init__(self, labels, compacter):
+    print("[CompacterBlock] Got labels", labels, "comp=", compacter)
     MasterBlock.__init__(self)
     self.labels = labels
     self.compacter = compacter
@@ -36,15 +38,14 @@ class CompacterBlock(MasterBlock):
     for k in self.results.keys():
       self.results[k] = []
 
-  def send(self,data):
+  def send(self, data):
     """Will actually send only once every self.compacter loops"""
-    #t = time.time()
-    #elapsed = t-self.t0
-    #self.results[self.labels[0]].append(elapsed)
-    for index,key in enumerate(self.labels):
+    # t = time.time()
+    # elapsed = t-self.t0
+    # self.results[self.labels[0]].append(elapsed)
+    for index, key in enumerate(self.labels):
       self.results[key].append(data[index])
     if len(self.results[self.labels[0]]) == self.compacter:
-      MasterBlock.send(self,self.results)
-      #print("sent",self.results)
+      MasterBlock.send(self, self.results)
+      # print("sent",self.results)
       self.clear_results()
-    
