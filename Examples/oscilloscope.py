e78456a01ef2afe461897af7bdb917d3b19cdc71
francoisbiar <francois.bari@gmail.com>
2017-01-20 09:36:19 +0100

    [update] Example with new crappy syntax

--- Examples/oscilloscope.py
+++ Examples/oscilloscope.py
@@ -1,100 +1,19 @@
-import time
+# Simple program to plot data. Use the concerned device.
+
 import crappy
 
-crappy.blocks.MasterBlock.instances = []  # Init masterblock instances
+channels = [0]
 
-# t0=time.time()
-try:
-    # Creating objects
-    # instronSensor=crappy.sensor.ComediSensor(device='/dev/comedi0',channels=[0,1],gain=[10,10])
-    sensor = crappy.sensor.ComediSensor(device='/dev/comedi0', channels=[0, 1, 2, 3],
-                                         gain=[1, 1, 1, 1], offset=[0.1723, 0.155, -0.005, 0.005])
-    # sensor = crappy.sensor.LabJackSensor(channels=[0], gain=[1], chan_range=10, mode="streamer", scanRate=10,
-    #                                       scansPerRead=5)
-    # sensor = crappy.sensor.LabJackSensor(channels=[0], gain=1, resolution=12, chan_range=10, mode="single")  #
-    # dist is multiplied by 2 to be correct
-    # 10 times the gain on the machine if you go through an usb dux sigma
-    # instronSensor = crappy.sensor.ComediSensor(device='/dev/comedi0',channels=[0,1],gain=[10,10000])
-    # cmd_traction=crappy.actuator.LabJackActuator(channel="TDAC2", gain=1, offset=0)
-    # cmd_traction2=crappy.actuator.LabJackActuator(channel="TDAC3", gain=1, offset=0)
-    # cmd_torsion = crappy.actuator.ComediActuator(device='/dev/comedi1', subdevice=1, channel=2, range_num=0, gain=1,
-    #                                               offset=0)
+# If comedi as sensor
+# sensor = crappy.sensor.ComediSensor(device='/dev/comedi0', channels=[0, 1, 2, 3],
+#                                      gain=[1, 1, 1, 1], offset=[0.1723, 0.155, -0.005, 0.005])
 
-    # Initialising the outputs
+# If labjack as sensor
+sensor = crappy.technical.LabJack(sensor={'channels': channels, 'gain': 1, 'offset': 0, 'resolution': 1}, verbose=True)
+stream = crappy.blocks.MeasureByStep(sensor, freq=1000, compacter=100, verbose=True, labels=['t(s)', str(channels[0])])
+graph = crappy.blocks.Grapher(('t(s)', str(channels[0])), length=0)
+dash = crappy.blocks.Dashboard(nb_digits=6)
 
-    # cmd_torsion.set_cmd(0)
-    # cmd_traction.set_cmd(0)
-
-    # Creating blocks
-    # send_freq=400, actuator=cmd_traction, waveform=['sinus','sinus','sinus'], freq=[0.5,2,1], time_cycles=[10,10,10], amplitude=[1,2,4], offset=[0,0,0], phase=[0,0,0], repeat=True
-    # send_freq=400, actuator=cmd_torsion, waveform=['sinus','triangle','sinus'], freq=[0.5,2,1], time_cycles=[10,10,10], amplitude=[0,0,0], offset=[0,0,0], phase=[np.pi,np.pi,np.pi], repeat=True
-    # stream=crappy.blocks.MeasureByStep(instronSensor,labels=['t(s)','signal','signal2'],freq=200)
-    # stream = crappy.blocks.MeasureByStep(sensor, labels=['t(s)', 'def(%)', 'F(N)', 'dist', 'C(Nm)'], freq=100)
-    stream = crappy.blocks.MeasureByStep(sensor, labels=['t(s)', 'AIN0', 'AIN1', 'AIN2', 'AIN3'], freq=100)
-    # stream=crappy.blocks.Streamer(sensor,labels=['t(s)','signal','signal2'])
-    # stream=crappy.blocks.MeasureComediByStep(instronSensor, labels=['t(s)','V'], freq=1000.)
-    # traction=crappy.blocks.SignalGenerator(path=[{"waveform":"sinus","time":100,"phase":0,"amplitude":1,"offset":0,"freq":2}],
-    # send_freq=400,repeat=True)
-    # torsion=crappy.blocks.SignalGenerator(path=[{"waveform":"triangle","time":50,"phase":0,"amplitude":5,"offset":-0.5,"freq":1}]
-    # ,send_freq=400,repeat=False,labels=['t(s)','signal'])
-
-    # send_output=crappy.blocks.CommandComedi([cmd_traction,cmd_traction2])
-    compacter = crappy.blocks.Compacter(20)
-    # compacter2=crappy.blocks.Compacter(400)
-    # save=crappy.blocks.Saver("/home/ilyesse/Bureau/delete_me3.txt")
-
-    # graph = crappy.blocks.Grapher(('t(s)', 'AIN0'))
-
-    graph = crappy.blocks.Grapher(('t(s)', 'AIN0'), ('t(s)', 'AIN1'), ('t(s)', 'AIN2'), ('t(s)', 'AIN3'), length=50)
-
-    # Creating links
-    # crappy.links.Filter(labels=['dist(deg)'],mode="median",size=50)
-    # condition=[crappy.links.Filter(labels=['V'],mode="median",size=50),crappy2.links.Filter(labels=['t(s)'],mode="mean",size=50)]
-    link1 = crappy.links.Link(name="stream_to_signalGenerator")
-
-    link3 = crappy.links.Link(name="link3")
-
-    # link5 = crappy.links.Link()
-    # link6 = crappy.links.Link()
-    # link7 = crappy.links.Link()
-
-    # Linking objects
-    stream.add_output(link1)
-    # traction.add_output(link2)
-    # traction.add_output(stream_to_signalGenerator)
-    # torsion.add_output(link5)
-
-    compacter.add_input(link1)
-    # compacter2.add_input(link2)
-    # send_output.add_input(link2)
-    compacter.add_output(link3)
-
-    # compacter.add_output(link5)
-    # compacter.add_output(link6)
-    # compacter.add_output(link7)
-
-    # save.add_input(link5)
-
-    graph.add_input(link3)
-
-    # graph_stat.add_input(link5)
-
-    # graph3.add_input(link7)
-    # Starting objects
-    t0 = time.time()
-    for instance in crappy.blocks.MasterBlock.instances:
-        instance.t0 = t0
-
-    for instance in crappy.blocks.MasterBlock.instances:
-        instance.start()
-
-        # Waiting for execution
-
-        # Stopping objects
-
-        # for instance in crappy.blocks.MasterBlock.instances:
-        # instance.stop()
-
-except KeyboardInterrupt:
-    for instance in crappy.blocks.MasterBlock.instances:
-        instance.stop()
+crappy.link(stream, dash)
+crappy.link(stream, graph)
+crappy.start()
