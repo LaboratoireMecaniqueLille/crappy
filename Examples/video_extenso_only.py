9825ef32c6dccc3aa40eadd84922292f9a343103
Victor Couty <victor@couty.eu>
2017-01-20 12:55:40 +0100

    [rew] video extenso example

--- Examples/video_extenso_only.py
+++ Examples/video_extenso_only.py
@@ -1,56 +1,15 @@
 import time
 import crappy
 
-crappy.blocks.MasterBlock.instances = []  # Init masterblock instances
-
-t0 = time.time()
 if __name__ == '__main__':
-    try:
 
-        # Creating blocks
-        compacter_extenso = crappy.blocks.Compacter(100)
-        save_extenso = crappy.blocks.Saver("/home/corentin/Bureau/delete2.txt")
-        graph_extenso = crappy.blocks.Grapher(('t(s)', 'Exx(%)'), ('t(s)', 'Eyy(%)'))
+  # Creating blocks
+  #save_extenso = crappy.blocks.Saver("DELME/delete2.txt")
+  graph_extenso = crappy.blocks.Grapher(('t(s)', 'Exx(%)'), ('t(s)', 'Eyy(%)'),length=0)
 
-        extenso = crappy.blocks.VideoExtenso(camera="ximea", numdevice=0, xoffset=0, yoffset=0, width=2048, height=2048,
-                                              white_spot=False, display=True)
+  extenso = crappy.blocks.VideoExtenso(camera="ximea", numdevice=0,
+                                xoffset=0, yoffset=0, width=2048, height=2048,
+                                white_spot=False, display=True, compacter=10)
 
-        # Creating links
-
-        link1 = crappy.links.Link()
-        link2 = crappy.links.Link()
-        link6 = crappy.links.Link()
-
-        link3 = crappy.links.Link()
-        link4 = crappy.links.Link()
-        link5 = crappy.links.Link()
-
-        # Linking objects
-        extenso.add_output(link3)
-        compacter_extenso.add_input(link3)
-        compacter_extenso.add_output(link4)
-        compacter_extenso.add_output(link5)
-
-        save_extenso.add_input(link4)
-
-        graph_extenso.add_input(link5)
-
-        # Starting objects
-        # print "top :",crappy.blocks._masterblock.MasterBlock.instances
-        t0 = time.time()
-        for instance in crappy.blocks.MasterBlock.instances:
-            instance.t0 = t0
-
-        for instance in crappy.blocks.MasterBlock.instances:
-            instance.start()
-
-            # Waiting for execution
-
-    # Stopping objects
-    except (Exception, KeyboardInterrupt) as e:
-        print "Exception in main :", e
-        for instance in crappy.blocks.MasterBlock.instances:
-            try:
-                instance.stop()
-            except Exception as e:
-                print e
+  crappy.link(extenso,graph_extenso)
+  crappy.start()
