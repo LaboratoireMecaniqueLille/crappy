4bb91fa06cd210aadde9ee60a08d6e6c23f38cbe
francoisbiar <francois.bari@gmail.com>
2017-01-30 16:25:50 +0100

    [update] added documentation

--- crappy/blocks/_wavegenerator.py
+++ crappy/blocks/_wavegenerator.py
@@ -8,7 +8,19 @@ from time import time, sleep
 
 
 class WaveGenerator(MasterBlock):
+  """
+  Block that sends simple waves.
+  """
   def __init__(self, *args, **kwargs):
+    """
+    Args:
+      waveform: str, could be: sin, triangle, square, pwm, saw_tooth.
+      nb_points: int, number of points per period.
+      wave_frequency: float, self explanatory
+      gain: gain of the signal generated
+      offset: offset of the signal.
+      duty_cycle: for pwm waveform only.
+    """
     MasterBlock.__init__(self)
     self.waveform = kwargs.get('waveform', 'sin')
     self.nb_points = kwargs.get('nb_points', 50)
@@ -20,6 +32,9 @@ class WaveGenerator(MasterBlock):
     self.duty_cycle = kwargs.get('duty_cycle', None)
 
   def sin(self):
+    """
+    sin generator function.
+    """
     x = np.linspace(0, 2 * np.pi, num=self.nb_points, endpoint=False)
     sin = np.sin(x)
     while True:
@@ -28,6 +43,9 @@ class WaveGenerator(MasterBlock):
         yield i * self.gain + self.offset
 
   def triangle(self):
+    """
+    triangle generator function.
+    """
     x = np.linspace(0, 1, num=self.nb_points, endpoint=True)
     while True:
       for i in x:
@@ -38,12 +56,18 @@ class WaveGenerator(MasterBlock):
         yield i * self.gain + self.offset
 
   def square(self):
+    """
+    square generator function.
+    """
     while True:
       for i in 0, 1:
         self.temporization(self.wave_frequency * 2)
         yield i * self.gain + self.offset
 
   def pwm(self):
+    """
+    pwm generator function.
+    """
     assert self.duty_cycle, 'Please define a duty cycle.'
     x1 = np.zeros(int((1 - self.duty_cycle) * self.nb_points))
     x2 = np.ones(int(self.duty_cycle * self.nb_points))
@@ -54,12 +78,18 @@ class WaveGenerator(MasterBlock):
         yield i * self.gain + self.offset
 
   def saw_tooth(self):
+    """
+    saw_tooth generator function.
+    """
     x = np.linspace(0, 1, num=self.nb_points, endpoint=True)
     while True:
       for i in x:
         yield i * self.gain + self.offset
 
   def temporization(self, timeout):
+    """
+    function called to temporize required time.
+    """
     t_a = time()
     while True:
       sleep(1 / (100 * timeout))
@@ -68,6 +98,9 @@ class WaveGenerator(MasterBlock):
         break
 
   def main(self):
+    """
+    Main loop for wavegenerator.
+    """
     function = getattr(self, self.waveform)()
     while True:
       current = function.next()
