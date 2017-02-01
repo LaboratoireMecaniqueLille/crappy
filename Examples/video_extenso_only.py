c5f797f9f1bc35393b11021711313d259796ef89
Victor Couty <victor@couty.eu>
2017-02-01 14:11:13 +0100

    [update] Updated videoextenso example

--- Examples/video_extenso_only.py
+++ Examples/video_extenso_only.py
@@ -1,14 +1,11 @@
-import time
 import crappy
 
 if __name__ == '__main__':
 
-  # Creating blocks
-  #save_extenso = crappy.blocks.Saver("DELME/delete2.txt")
-  graph_extenso = crappy.blocks.Grapher(('t(s)', 'Exx(%)'), ('t(s)', 'Eyy(%)'),length=0)
+  graph_extenso = crappy.blocks.Grapher(('t(s)', 'Exx(%)'), ('t(s)', 'Eyy(%)'),
+                                        length=0)
 
   extenso = crappy.blocks.VideoExtenso(camera="XimeaCV", compacter=10)
-            #height=1024,width=1024,xoffset=512,yoffset=512)
 
   crappy.link(extenso,graph_extenso)
   crappy.start()
