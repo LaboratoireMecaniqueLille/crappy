08d51918db497c145839c85999c7b0b0c76e8330
Victor Couty <victor@couty.eu>
2017-02-01 10:33:40 +0100

    [ref] Rewritten biaxe exemple

--- Examples/biaxe.py
+++ Examples/biaxe.py
@@ -1,148 +1,88 @@
-import time
+from __future__ import division
 import crappy
-
-crappy.blocks.MasterBlock.instances = []  # Init masterblock instances
+import time
 
 # conversion : 1 speed = 0.002 mm/s
 
-t0 = time.time()
+def mean(l):
+    return sum(l)/len(l)
+
+class Mean(crappy.links.Condition):
+    def __init__(self,nb_points=20):
+        pass
+        
+        
+    def evaluate(self,data):
+        for k in data:
+            print k
+            val = data[k]
+            nb = len(val)
+            data[k] = [mean(val[:nb//2]),mean(val[nb//2:])]
+        return data
+ 
+def eval_offset(device, duration):
+    """
+    Method to evaluate offset. Returns an average of measurements made during specified time.
+    """
+    timeout = time.time() + duration  # secs from now
+    print('Measuring offset (%d sec), please wait...'%duration)
+    offsets = []
+
+
+    while True:
+        chan1 = device.get_data('all')[1]
+        offsets.append(chan1)
+        if time.time() > timeout:
+            offsets = -np.mean(offset)
+        print('offset:', offsets)
+        break
+    return offsets
+
 if __name__ == '__main__':
-    try:
-        # Creating objects
+    loading_path_list = []
+    d = {"waveform": "limit", "gain": 1, "cycles": 1, "phase": 0,
+                           "lower_limit": [0.1, 'F(N)']}
 
-        instronSensor = crappy.sensor.ComediSensor(channels=[1, 3], gain=[-3749.3, -3198.9 * 1.18], offset=[0, 0])
-        t, F2 = instronSensor.get_data(0)
-        t, F4 = instronSensor.get_data(1)
-        instronSensor = crappy.sensor.ComediSensor(channels=[1, 3], gain=[-3749.3, -3198.9 * 1.18], offset=[-F2, -F4])
-        biaxeTech1 = crappy.technical.Biaxe(port='/dev/ttyS4')
-        biaxeTech2 = crappy.technical.Biaxe(port='/dev/ttyS5')
-        biaxeTech3 = crappy.technical.Biaxe(port='/dev/ttyS6')
-        biaxeTech4 = crappy.technical.Biaxe(port='/dev/ttyS7')
+    for i in range(1,10):
+      loading_path_list.append(d.copy())
+      loading_path_list[-1].update({"upper_limit": [i*10,'Exx(%)']})
 
-        axes = [biaxeTech1, biaxeTech2, biaxeTech3, biaxeTech4]
+    loading_path_list.append({'waveform': 'hold', 'time': 5})
+    timestamp = time.ctime()[:-5].replace(" ","_")
+    savepath = "./"+timestamp+"/"
+    Fsensor = crappy.sensor.ComediSensor(channels=[1], gain=[-3749.3], offset=[0])
+    off1 = eval_offset(Fsensor,1)
+    Fsensor = crappy.sensor.ComediSensor(channels=[1], gain=[-3749.3], offset=[-off1[0][0]])
+    print "F:",Fsensor.get_data()
 
-        # Creating blocks
+    biaxeTech1 = crappy.technical.Biaxe(port='/dev/ttyS4')
+    biaxeTech2 = crappy.technical.Biaxe(port='/dev/ttyS5')
 
-        compacter_effort = crappy.blocks.Compacter(200)
-        save_effort = crappy.blocks.Saver("/home/biaxe/Bureau/Annie/effort.txt")
-        graph_effort = crappy.blocks.Grapher(('t(s)', 'F2(N)'), ('t(s)', 'F4(N)'))
+    # Creating blocks
+    save_effort = crappy.blocks.Saver(savepath+"force.csv")
+    graph_effort = crappy.blocks.Grapher(('t(s)', 'F(N)'),length=0)
+    save_extenso = crappy.blocks.Saver(savepath+"extenso.csv")
+    graph_extenso = crappy.blocks.Grapher(('t(s)', 'Exx(%)'), ('t(s)', 'Eyy(%)'),length=0)
 
-        compacter_extenso = crappy.blocks.Compacter(90)
-        save_extenso = crappy.blocks.Saver("/home/biaxe/Bureau/Annie/extenso.txt")
-        graph_extenso = crappy.blocks.Grapher(('t(s)', 'Exx(%)'), ('t(s)', 'Eyy(%)'))
+    effort = crappy.blocks.MeasureByStep(Fsensor, labels=['t(s)', 'F(N)'], freq=200, 
+        white_spots=False,compacter=100)
+    extenso = crappy.blocks.VideoExtenso(camera="XimeaCV", white_spot=False, display=True, compacter=3)
 
-        effort = crappy.blocks.MeasureComediByStep(instronSensor, labels=['t(s)', 'F2(N)', 'F4(N)'], freq=200)
-        extenso = crappy.blocks.VideoExtenso(camera="Ximea", xoffset=0, yoffset=0, width=2048, height=2048,
-                                              white_spot=True, display=True)
+    signal_generator = crappy.blocks.SignalGenerator(
+        path=loading_path_list,
+        send_freq=100, repeat=False, labels=['t(s)', 'signal'])
 
-        # signalGenerator=crappy.blocks.SignalGenerator(path=[{"waveform":"hold","time":3},
-        # {"waveform":"limit","cycles":3,"phase":0,"lower_limit":[50,'F(N)'],"upper_limit":[5,'Exx(%)']}],
-        # send_freq=400,repeat=True,labels=['t(s)','signal'])
+    biaxe = crappy.blocks.CommandBiaxe(biaxe_technicals=[biaxeTech1, biaxeTech2], speed=-1000)  # vertical
 
-        # signalGenerator=crappy.blocks.SignalGenerator(path=[{"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[50,'F2(N)'],"upper_limit":[10,'Exx(%)']},
-        # {"waveform":"limit","gain":0,"cycles":0.5,"phase":0,"lower_limit":[50,'F4(N)'],"upper_limit":[9.7,'Eyy(%)']},
-        # {"waveform":"limit","gain":1,"cycles":0.5,"phase":-np.pi,"lower_limit":[50,'F2(N)'],"upper_limit":[10,'Exx(%)']}],
-        # send_freq=400,repeat=True,labels=['t(s)','signal'])
-        signalGenerator = crappy.blocks.SignalGenerator(
-            path=[{"waveform": "limit", "gain": 1, "cycles": 0.5, "phase": 0, "lower_limit": [0.1, 'F2(N)'],
-                   "upper_limit": [200, 'F(N)']}],
-            # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[80,'Exx(%)']},
-            # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[120,'Exx(%)']},
-            # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[160,'Exx(%)']},
-            # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[200,'Exx(%)']}],
-            send_freq=400, repeat=True, labels=['t(s)', 'signal'])
+    crappy.link(effort,save_effort)
+    crappy.link(effort,graph_effort)
+    crappy.link(effort,signal_generator)
 
-        # signalGenerator_horizontal=crappy.blocks.SignalGenerator(path=[{"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[40,'Exx(%)']},
-        # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[80,'Exx(%)']},
-        # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[120,'Exx(%)']},
-        # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[160,'Exx(%)']},
-        # {"waveform":"limit","gain":1,"cycles":0.5,"phase":0,"lower_limit":[10,'F2(N)'],"upper_limit":[200,'Exx(%)']}],
-        # send_freq=400,repeat=True,labels=['t(s)','signal'])
+    crappy.link(extenso,save_extenso)
+    crappy.link(extenso,graph_extenso)
+    crappy.link(extenso,signal_generator)
 
-        biotens = crappy.blocks.CommandBiaxe(biaxe_technicals=[biaxeTech1, biaxeTech2], speed=-0)  # vertical
-        biotens_horizontal = crappy.blocks.CommandBiaxe(biaxe_technicals=[biaxeTech3, biaxeTech4], speed=-167)
-        # horizontal # speed must be <0 for traction
+    crappy.link(signal_generator,biaxe)
 
 
-        # Creating links
-
-        link1 = crappy.links.Link()
-        link2 = crappy.links.Link()
-        link12 = crappy.links.Link()
-        link22 = crappy.links.Link()
-        link3 = crappy.links.Link()
-        link4 = crappy.links.Link()
-        link5 = crappy.links.Link()
-        link6 = crappy.links.Link()
-        link7 = crappy.links.Link()
-        link8 = crappy.links.Link()
-        link9 = crappy.links.Link()
-        link92 = crappy.links.Link()
-
-        # Linking objects
-
-        effort.add_output(link1)
-        effort.add_output(link12)
-        effort.add_output(link6)
-
-        extenso.add_output(link2)
-        extenso.add_output(link22)
-        extenso.add_output(link3)
-
-        signalGenerator.add_input(link1)
-        signalGenerator.add_input(link2)
-        signalGenerator.add_output(link9)
-
-        # signalGenerator_horizontal.add_input(link12)
-        # signalGenerator_horizontal.add_input(link22)
-        # signalGenerator_horizontal.add_output(link92)
-
-        biotens.add_input(link9)
-        biotens_horizontal.add_input(link92)
-
-        compacter_effort.add_input(link6)
-        compacter_effort.add_output(link7)
-        compacter_effort.add_output(link8)
-
-        save_effort.add_input(link7)
-
-        graph_effort.add_input(link8)
-
-        compacter_extenso.add_input(link3)
-        compacter_extenso.add_output(link4)
-        compacter_extenso.add_output(link5)
-
-        save_extenso.add_input(link4)
-
-        graph_extenso.add_input(link5)
-
-        # Starting objects
-        # print "top :",crappy.blocks._masterblock.MasterBlock.instances
-        t0 = time.time()
-        for instance in crappy.blocks.MasterBlock.instances:
-            instance.t0 = t0
-
-        for instance in crappy.blocks.MasterBlock.instances:
-            instance.start()
-
-    # Waiting for execution
-
-
-    # Stopping objects
-
-    except (Exception, KeyboardInterrupt) as e:
-        print "Exception in main :", e
-        for instance in crappy.blocks.MasterBlock.instances:
-            try:
-                instance.stop()
-            except:
-                pass
-        for axe in axes:
-            try:
-                axe.close()
-            except:
-                pass
-
-
-                # for manual setting :
-                # biaxeTech3.actuator.set_speed(500);biaxeTech4.actuator.set_speed(500);biaxeTech1.actuator.set_speed(500);biaxeTech2.actuator.set_speed(500)
+    crappy.start()
