5e7a64dee8f0241d123abbecc0b1dee012ecf066
francoisbiar <francois.bari@gmail.com>
2017-02-02 10:05:52 +0100

    [update] cleaned useless statements

--- crappy/sensor/_dummySensor.py
+++ crappy/sensor/_dummySensor.py
@@ -25,40 +25,26 @@ class DummySensor(acquisition.Acquisition, motion.MotionSensor):
 
   def __init__(self, *args, **kwargs):
     super(DummySensor, self).__init__()
-    self.args = args
-    self.kwargs = kwargs
     self.position = random.random() * random.random() * 10
-    if "channels" in kwargs:
-      self.channels = kwargs.get("channels")
-      if not isinstance(self.channels, list):
-        self.channels = [self.channels]
-    else:
-      self.channels = [0]
-    if "offset" in kwargs:
-      self.offset = kwargs.get("offset")
-      if not isinstance(self.offset, list):
-        self.offset = [self.offset] * len(self.channels)
-    else:
-      self.offset = [0]
-    if "gain" in kwargs:
-      self.gain = kwargs.get("gain")
-      if not isinstance(self.gain, list):
-        self.gain = [self.gain] * len(self.channels)
-    else:
-      self.gain = [0]
-    if "ret_type" in kwargs:
-      self.ret_type = kwargs.get("ret_type")
-    else:
-      self.ret_type = "random"
-
+    self.channels = kwargs.get("channels", [0])
+    if not isinstance(self.channels, list):
+      self.channels = [self.channels]
+    self.offset = kwargs.get("offset", [0] * len(self.channels))
+    if not isinstance(self.offset, list):
+      self.offset = [self.offset] * len(self.channels)
+    self.gain = kwargs.get("gain", [1] * len(self.channels))
+    if not isinstance(self.gain, list):
+      self.gain = [self.gain] * len(self.channels)
+    self.ret_type = kwargs.get("ret_type", "random")
     self.infinite_iterator = self.frange_infinite(0, 1)
+
     if self.ret_type == "sin":
       self.iterator = self.frange(-math.pi / 2, math.pi / 2, 0.1)
     elif self.ret_type == "cos":
       self.iterator = self.frange(-math.pi, math.pi, 0.1)
     else:
       self.iterator = self.frange(-10, 10, 0.1)
-
+      print('gain and offset:', self.gain, self.offset)
   @staticmethod
   def frange(x, y, jump):
     a = x
@@ -86,7 +72,6 @@ class DummySensor(acquisition.Acquisition, motion.MotionSensor):
     """
     Read the signal for desired channel
     """
-    ret_value = 0
     if self.ret_type == "sin":
       ret_value = math.sin(self.iterator.next())
     elif self.ret_type == "cos":
@@ -99,7 +84,7 @@ class DummySensor(acquisition.Acquisition, motion.MotionSensor):
       ret_value = round(random.random() * random.random() * 10, 3)
     if channel_number == "all":
       result = []
-      for i in range(len(self.channels)):
+      for i in xrange(len(self.channels)):
         result.append(ret_value * self.gain[i] + self.offset[i])
       t = time.time()
       return t, result
