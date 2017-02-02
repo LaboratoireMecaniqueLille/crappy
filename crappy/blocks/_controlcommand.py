8b0b8dc24d6881e72ddcb6a766cc6dfb7a772713
francoisbiar <francois.bari@gmail.com>
2017-02-02 10:07:22 +0100

    [update] new lua script method

--- crappy/blocks/_controlcommand.py
+++ crappy/blocks/_controlcommand.py
@@ -9,9 +9,22 @@ import time
 # Outputs : the signal, and the measures.
 
 class ControlCommand(MeasureByStep):
+  """
+  Class to control and command from a single device. Works for labjack
+  at the moment.
+  """
   def __init__(self, device, *args, **kwargs):
+    """
+    Args :
+      device: which device to control and command.
+      verbose: prints the acquisition frequency if true.
+      lua: used to control lua scripts. Specify a dictionary that
+      will be initiated.
+      The other arguments are the same as the MeasureByStep block.
+    """
     self.verbose = kwargs.get('verbose', False)
     self.device = device
+    self.lua = kwargs.get('lua', False)
     MeasureByStep.__init__(self, sensor=self.device, *args, **kwargs)
 
   def vprint(*args):
@@ -24,39 +37,62 @@ class ControlCommand(MeasureByStep):
     MeasureByStep.prepare(self)
     print(self.inputs, self.outputs)
     if len(self.inputs) == 0 and len(self.outputs) > 0:
-      pass  # because this is already the measurebystep.main.
+      pass
+
     elif len(self.inputs) == 1 and len(self.outputs) == 0:
       self.main = self.main_command
+
     elif len(self.inputs) == 1 and len(self.outputs) > 0:
-      self.main = self.main_control_command
+      if self.lua:
+        self.main = self.main_control_command_lua
+      else:
+        self.main = self.main_control_command
+
       self.labels.append('signal')
       self.clear_results()
       if self.verbose:
         self.prepare_verbosity()
     else:
-      raise Exception('Wrong definition of ControlCommand block.')
+      raise TypeError('Wrong definition of ControlCommand block.')
 
   def main_command(self):
 
     while True:
-      command = self.inputs[0].recv()['signal']
+      command = self.inputs[0].recv(blocking=True)['signal']
       self.device.set_cmd(command)
 
   def main_control_command(self):
 
     while True:
-
       data = self.acquire_data()
-      command = self.inputs[0].recv(blocking=True)['signal']
-
-      data.append(command)
-
+      command = self.inputs[0].recv(blocking=False)
+      if command:
+        signal = command['signal']
+        self.device.set_cmd(signal)
+      else:
+        signal = 0.0
+      data.append(signal)
       self.send_to_compacter(data)
-      self.device.set_cmd(command)
+      if self.verbose:
+        self.increment_verbosity(data)
+
+  def increment_verbosity(self, data):
+    self.nb_acquisitions += 1
+    time_interval = data[0] - self.elapsed
+    if time_interval >= 1.:
+      self.elapsed = data[0]
+      self.queue.put(self.nb_acquisitions)
+
+  def main_control_command_lua(self):
+
+    while True:
+      data = self.acquire_data()
+      received = self.inputs[0].recv(blocking=False)
+
+      if received:
+        self.sensor.set_parameter_ram(received)
+      data.append(0)
+      self.send_to_compacter(data)
 
       if self.verbose:
-        self.nb_acquisitions += 1
-        time_interval = data[0] - self.elapsed
-        if time_interval >= 1.:
-          self.elapsed = data[0]
-          self.queue.put(self.nb_acquisitions)
+        self.increment_verbosity(data)
