f5f8cc1c54ee5d311d012d90c20580c26d3b17bc
francoisbiar <francois.bari@gmail.com>
2017-02-08 17:41:07 +0100

    [update] minor changes and comments

---
+++ crappy/blocks/_rapher.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+##  @addtogroup blocks
+# @{
+
+##  @defgroup Grapher Grapher
+# @{
+
+## @file _grapher.py
+# @brief The grapher plots data received from the Compacter (via a Link).
+#
+# @author Robin Siemiatkowski
+# @version 0.1
+# @date 11/07/2016
+
+from _masterblock import MasterBlock
+# Major library imports
+from numpy import arange
+from scipy.special import jn
+
+# Enthought library imports
+from enable.api import Window
+from enable.example_support import DemoFrame, demo_main
+from traits.api import HasTraits
+from pyface.timer.api import Timer
+
+# Chaco imports
+from chaco.api import create_line_plot, OverlayPlotContainer
+from chaco.tools.api import MoveTool, PanTool, ZoomTool
+
+class Rapher(MasterBlock, ):
+  """Plot the input data"""
+
+  def __init__(self, *args, **kwargs):
+    pass
+
+
