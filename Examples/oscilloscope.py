ab92f46d88c8649c78b972c37ba60eea512195e1
francoisbiar <francois.bari@gmail.com>
2017-03-06 10:34:55 +0100

    [update] example with new syntax

--- Examples/oscilloscope.py
+++ Examples/oscilloscope.py
@@ -5,16 +5,22 @@ import crappy
 channels = [0]
 
 # If comedi as sensor
-# sensor = crappy.sensor.ComediSensor(device='/dev/comedi0', channels=[0, 1, 2, 3],
-#                                      gain=[1, 1, 1, 1], offset=[0.1723, 0.155, -0.005, 0.005])
+sensor = crappy.sensor.ComediSensor(device='/dev/comedi0',
+                                    channels=[0, 1, 2, 3],
+                                    gain=[1, 1, 1, 1],
+                                    offset=[0.1723, 0.155, -0.005, 0.005])
 
 # If labjack as sensor
-sensor = crappy.technical.LabJack(sensor={'channels': channels, 'gain': 1, 'offset': 0, 'resolution': 1}, verbose=True)
+sensor = crappy.technical.LabJack(
+  sensor={'channels': channels, 'gain': 1, 'offset': 0, 'resolution': 1},
+  verbose=True)
 
 # If openDAQ as sensor
 # sensor = crappy.technical.OpenDAQ()
 
-stream = crappy.blocks.MeasureByStep(sensor, freq=None, compacter=1000, verbose=True, labels=['t(s)', str(channels[0])])
+stream = crappy.blocks.MeasureByStep(sensor, freq=None, compacter=1000,
+                                     verbose=True,
+                                     labels=['t(s)', str(channels[0])])
 
 graph = crappy.blocks.Grapher(('t(s)', str(channels[0])), length=1)
 dash = crappy.blocks.Dashboard(nb_digits=6)
--- Examples/oscilloscope_thermocouples.py
+++ Examples/oscilloscope_thermocouples.py
@@ -1,42 +1,18 @@
-import time
-import numpy as np
+"""
+Simple example to show how to use labjack devices for thermocouple measures.
+"""
 import crappy
 
-crappy.blocks.MasterBlock.instances = []  # Init masterblock instances
+sensor = crappy.sensor.LabJackSensor(channels=["AIN1"],
+                                     mode="thermocouple")
 
-try:
+stream = crappy.blocks.MeasureByStep(sensor,
+                                     labels=['t(s)', 'T'],
+                                     verbose=True)
 
-	sensor = crappy.sensor.LabJackSensor(channels=["AIN0", "AIN1", "AIN2", "AIN3"], mode="thermocouple")
+graph = crappy.blocks.Grapher(('t(s)', 'T'), length=180)  #
+dash = crappy.blocks.Dashboard(nb_digits=1)
 
-	stream = crappy.blocks.MeasureByStep(sensor, labels=['t(s)', 'T', 'T2', 'T3', 'T4'], freq=800)
-
-	compacter = crappy.blocks.Compacter(20)
-	# save = crappy.blocks.Saver("/home/corentin/Code/crappy/tests_francois/Thermocouples_20160621/"
-	#                            "thermocouples_instron_1Hz_30Nm.txt")
-
-	graph = crappy.blocks.Grapher(('t(s)', 'T'), ('t(s)', 'T2'), ('t(s)', 'T3'), ('t(s)', 'T4'), length=180)  #
-	# Links
-	Link_StreamToCompacter = crappy.links.Link()  # Link_StreamToCompacter = crappy.links.Link()  #
-	stream.add_output(Link_StreamToCompacter)
-	compacter.add_input(Link_StreamToCompacter)
-	#
-	Link_CompacterToGraph = crappy.links.Link()
-	compacter.add_output(Link_CompacterToGraph)
-	graph.add_input(Link_CompacterToGraph)
-	#
-	# Link_CompacterToSave = crappy.links.Link()
-	# compacter.add_output(Link_CompacterToSave)
-	# save.add_input(Link_CompacterToSave)
-
-	# Starting objects
-	t0 = time.time()
-
-	for instance in crappy.blocks.MasterBlock.instances:
-		instance.t0 = t0
-
-	for instance in crappy.blocks.MasterBlock.instances:
-		instance.start()
-
-except KeyboardInterrupt:
-	for instance in crappy.blocks.MasterBlock.instances:
-		instance.stop()
+crappy.link(stream, graph)
+crappy.link(stream, dash)
+crappy.start()
