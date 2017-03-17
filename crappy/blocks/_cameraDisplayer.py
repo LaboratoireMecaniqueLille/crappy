c5a31099cdd61ee217fbf53b88d9a25cda0f2d9a
Victor Couty <victor@couty.eu>
2017-03-17 09:36:27 +0100

    [FIX] CameraDisplayer can handle images with more than 8 bits depth

--- crappy/blocks/_cameraDisplayer.py
+++ crappy/blocks/_cameraDisplayer.py
@@ -15,16 +15,18 @@
 # @date 05/07/2016
 
 from _masterblock import MasterBlock
+import numpy as np
 
 
 class CameraDisplayer(MasterBlock):
   """
   Simple images displayer. Can be paired with StreamerCamera
   Use cv=False to use the old, inefficient and deprecated version
-  NOTE: You need to use one displayer block per window (in other words, you can only attach one input to the diplayer)
+  NOTE: You need to use one displayer block per window
+  (in other words, you can only attach one input to the diplayer)
   """
   def __init__(self, framerate=5, cv=True, title='Displayer'):
-    super(CameraDisplayer, self).__init__()
+    MasterBlock.__init__(self)
     if framerate is None:
       self.delay = 0
     else:
@@ -47,8 +49,16 @@ class CameraDisplayer(MasterBlock):
     fig = self.plt.figure()
     fig.add_subplot(111)
 
+  def cast_8bits(self,f):
+    m = f.max()
+    i = 0
+    while m >= 2**(8+i):
+      i+=1
+    print("DEBUG: divide by",2**i)
+    return (f/(2**i)).astype(np.uint8)
+
   def loop_mpl(self):
-    frame = self.inputs[0].recv()['frame']
+    frame = self.inputs[0].recv()['frame'][-1]
     self.plt.imshow(frame, cmap='gray')
     self.plt.pause(0.001)
     self.plt.show()
@@ -58,12 +68,18 @@ class CameraDisplayer(MasterBlock):
     self.cv2 = cv2
     try:
       flags = self.cv2.WINDOW_NORMAL | self.cv2.WINDOW_KEEPRATIO
-    except AttributeError:  # WINDOW_KEEPRATIO is not implemented in all opencv versions...
+    # WINDOW_KEEPRATIO is not implemented in all opencv versions...
+    except AttributeError:
       flags = self.cv2.WINDOW_NORMAL
     self.cv2.namedWindow(self.title, flags)
 
   def loop_cv(self):
     data = self.inputs[0].recv_delay(self.delay)['frame'][-1]
+    if data.dtype != np.uint8:
+      if data.max() >= 256:
+        data = self.cast_8bits(data)
+      else:
+        data = data.astype(np.uint8)
     self.cv2.imshow(self.title,data)
     self.cv2.waitKey(1)
 
