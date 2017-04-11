d274128781bfca62b804c4fceff97770fe0c610d
Victor Couty <victor@couty.eu>
2017-04-11 16:34:03 +0200

    [del] Removed unsupported examples

--- Examples/daqmx.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-
-import crappy
-
-if __name__ == '__main__':
-  s = crappy.blocks.DataReader(device='Dev2')
-  g = crappy.blocks.Grapher(('t','V'),length=0, compacter=100)
-  crappy.link(s,g)
-  crappy.start()
