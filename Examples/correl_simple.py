70867a18c4c80bbe710f1faf79ad0ec2c86c0667
Victor Couty <victor@couty.eu>
2017-04-19 09:35:46 +0200

    [add] Examples for correlation

---
+++ Examples/correl_simple.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+import crappy
+
+graph = crappy.blocks.Grapher(('t(s)','x'),('t(s)','y'),('t(s)','r'),length=50)
+
+correl = crappy.blocks.Correl(camera="XimeaCV",fields=['x','y','r']) # Rigid body
+
+crappy.link(correl,graph)
+
+crappy.start()
