927bd417388a0eb2f4ad99f5e09299d07a4e8590
Victor Couty <victor@couty.eu>
2017-04-20 10:16:56 +0200

    [add] cleanup script to remove compiled data and crappy packages

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "This script will remove every compiled files, pyc and dist-packages"
+echo "related to Crappy. RUN AT YOUR OWN RISK"
+echo "This script requires trash-cli package and sudo access"
+echo "Continue?"
+read conf
+if [ "$conf" != "y" ];then
+  exit 0
+fi
+sudo trash-put ~/crappy/build ~/crappy/dist ~/crappy/crappy.egg-info
+sudo trash-put /usr/local/lib/python2.7/dist-packages/crappy-*
+find . -name "*~" -delete
+n=`find . -name "*.pyc" |wc -l`
+if [ $n = 0 ];then
+  echo "No pyc files found, exiting..."
+  exit 0
+fi
+find . -name "*.pyc"
+echo "Delete these $n .pyc files? (y/[n])"
+read conf
+if [ "$conf" = "y" ];then
+  find . -name "*.pyc" -delete
+fi
