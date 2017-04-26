4e56761acefe7672af16d7551cc6708728628840
Victor Couty <victor@couty.eu>
2017-04-26 10:22:08 +0200

    [add] constant path can hold previous value

--- crappy/blocks/generator_path/constant.py
+++ crappy/blocks/generator_path/constant.py
@@ -7,10 +7,10 @@ class Constant(Path):
   """
   Simplest condition: will send value unil condition is reached and end
   """
-  def __init__(self,time,cmd,condition,value):
+  def __init__(self,time,cmd,condition,value=None):
     Path.__init__(self,time,cmd)
     self.condition = self.parse_condition(condition)
-    self.value = value
+    self.value = cmd if value is None else value
 
   def get_cmd(self,data):
     if self.condition(data):
