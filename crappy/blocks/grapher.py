b38c9db6bf4fd469fe224b7ccf2ecf9263d565dd
François Bari <francois.bari@gmail.com>
2017-06-13 11:47:36 +0200

    [fix] re-added window_pos and window_size options

--- crappy/blocks/grapher.py
+++ crappy/blocks/grapher.py
@@ -1,9 +1,11 @@
-#coding: utf-8
+# coding: utf-8
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from .masterblock import MasterBlock
+
+
 class Grapher(MasterBlock):
   """
   The grapher receive data from a block (via a Link) and plots it.
@@ -25,6 +27,12 @@ class Grapher(MasterBlock):
           Self explanatory ?
       freq: Defines the refresh rate of the grapher
 
+      optional: window_pos (tuple: (x_position, y_position), in PIXELS).
+          Defines where on the screen the window grapher pops. Works with
+          multiple screens. The origin is the top left corner. For instance,
+          to have a graph that pops on the top right corner:
+          window_pos=(1920, 0)
+
   Examples
   --------
       graph=Grapher(('t(s)','F(N)'),('t(s)','def(%)'))
@@ -34,31 +42,39 @@ class Grapher(MasterBlock):
       graph=Grapher(('t(s)','F(N)'),length=30)
           plot a dynamic graph that will display the last 30 chunks of data
   """
-  def __init__(self,*args,**kwargs):
+
+  def __init__(self, *args, **kwargs):
     MasterBlock.__init__(self)
-    self.length = kwargs.pop("length",0)
-    self.freq = kwargs.pop("freq",5)
-    self.maxpt = kwargs.pop("maxpt",20000)
+    self.length = kwargs.pop("length", 0)
+    self.freq = kwargs.pop("freq", 5)
+    self.maxpt = kwargs.pop("maxpt", 20000)
+    self.window_size = kwargs.pop("window_size", (8, 8))
+    self.window_pos = kwargs.pop("window_pos", None)
     if kwargs:
-      raise AttributeError("Invalid kwarg(s) in Grapher: "+str(kwargs))
+      raise AttributeError("Invalid kwarg(s) in Grapher: " + str(kwargs))
     self.labels = args
 
   def prepare(self):
-    self.f = plt.figure(figsize=(8,8))
+    self.f = plt.figure(figsize=self.window_size)
     self.ax = self.f.add_subplot(111)
     self.lines = []
     for _ in self.labels:
-      self.lines.append(self.ax.plot([],[])[0])
-    legend = [y for x,y in self.labels]
+      self.lines.append(self.ax.plot([], [])[0])
+    legend = [y for x, y in self.labels]
     plt.legend(legend, bbox_to_anchor=(-0.03, 1.02, 1.06, .102), loc=3,
-                    ncol=len(legend), mode="expand", borderaxespad=1)
+               ncol=len(legend), mode="expand", borderaxespad=1)
+    plt.xlabel(self.labels[0][0])
+    plt.ylabel(self.labels[0][1])
     plt.grid()
+    if self.window_pos:
+      mng = plt.get_current_fig_manager()
+      mng.window.wm_geometry("+%s+%s" % self.window_pos)
 
   def loop(self):
     data = self.get_all_last()
-    for i,(lx,ly) in enumerate(self.labels):
-      x = np.hstack((self.lines[i].get_xdata(),data[lx]))
-      y = np.hstack((self.lines[i].get_ydata(),data[ly]))
+    for i, (lx, ly) in enumerate(self.labels):
+      x = np.hstack((self.lines[i].get_xdata(), data[lx]))
+      y = np.hstack((self.lines[i].get_ydata(), data[ly]))
       if self.length and len(x) >= self.length:
         x = x[-self.length:]
         y = y[-self.length:]
@@ -67,7 +83,7 @@ class Grapher(MasterBlock):
         y = y[::2]
       self.lines[i].set_xdata(x)
       self.lines[i].set_ydata(y)
-    self.ax.autoscale_view(True,True,True)
+    self.ax.autoscale_view(True, True, True)
     self.f.canvas.draw()
     self.ax.relim()
     plt.pause(.01)
