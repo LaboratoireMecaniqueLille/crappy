d4dbeed22d281a5bca142ff6b082f77910284006
Victor Couty <victor@couty.eu>
2017-06-27 14:00:35 +0200

    [add] fake motor actuator

--- crappy/actuator/__init__.py
+++ crappy/actuator/__init__.py
@@ -4,5 +4,6 @@ from .actuator import MetaActuator,Actuator
 from .biaxe import Biaxe
 from .biotens import Biotens
 from .cmDrive import CM_drive
+from .fakemotor import Fake_motor
 from .oriental import Oriental
 actuator_list = MetaActuator.classes
