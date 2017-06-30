153ea694228e884001600107b2b47850354d2207
Victor Couty <victor@couty.eu>
2017-06-30 09:37:24 +0200

    [fix] Made Spectrum a facultative module

--- crappy/inout/__init__.py
+++ crappy/inout/__init__.py
@@ -8,7 +8,10 @@ from .inout import InOut,MetaIO
 from .agilent34420A import Agilent34420A
 from .arduino import Arduino
 from .opsens import Opsens
-from .spectrum import Spectrum
+try:
+  from .spectrum import Spectrum
+except (ImportError,OSError):
+  Spectrum = NotInstalled("Spectrum")
 try:
   from .comedi import Comedi
 except ImportError:
