f17d4918b3bb11baace24c5a7c0ea0c9e39f974d
Victor Couty <victor@couty.eu>
2017-09-04 15:43:49 +0200

    [add] Const parameter for inertia simulation

--- crappy/blocks/generator_path/inertia.py
+++ crappy/blocks/generator_path/inertia.py
@@ -9,9 +9,9 @@ class Inertia(Path):
   Used to lower/higher the output command by integrating an input over time
 
   Let f(t) be the input signal, v(t) the value of the output,
-  m the inertia and t0 the beginning of this path.
+  m the inertia and t0 the beginning of this path. K is a choosen constant.
   The output value for this path will be:
-  v(t) = v(t0) - [I(t0->t)f(t)dt]/m
+  v(t) = v(t0) - K*[I(t0->t)f(t)dt]/m
 
   Args:
   inertia (float):
@@ -26,15 +26,20 @@ class Inertia(Path):
       In other word, there must be exactly the same number of values
       received by these two labels at any instant
       (ie they must come from the same parent block).
+  const (float), default is 30/pi:
+    The value of K in the formula above. The default value is meant to send
+    rpm with inertia in kgm² and torque in Nm. If sending rad/s, use const=1
   condition (str, see Path for more info):
     Condition to meet to end this path
   """
-  def __init__(self,time,cmd,condition,inertia,flabel,tlabel='t(s)',value=None):
+  def __init__(self,time,cmd,condition,inertia,
+      flabel,const=30/np.pi,tlabel='t(s)',value=None):
     Path.__init__(self,time,cmd)
     self.condition = self.parse_condition(condition)
     self.inertia = inertia
     self.flabel = flabel
     self.tlabel = tlabel
+    self.const = const/self.inertia
     self.value = cmd if value is None else value
     self.last_t = None
 
@@ -43,7 +48,7 @@ class Inertia(Path):
       raise StopIteration
     if data[self.tlabel]:
       if self.last_t is None:
-        # If it is the first call, we cannot is the first data point, since we
+        # If it is the first call, we cannot use the first data point, since we
         # don't have the previous time (I use left rectangle integration)
         t = data[self.tlabel]
         if len(t) == 1:
@@ -57,6 +62,6 @@ class Inertia(Path):
         t = [self.last_t]+data[self.tlabel] # We have a previous point: use it
         f = np.array(data[self.flabel])
       dt = np.array([j-i for i,j in zip(t[:-1],t[1:])])
-      self.value -= sum(dt*f)/self.inertia # The actual integration
+      self.value -= self.const*sum(dt*f) # The actual integration
       self.last_t = t[-1]
     return self.value
