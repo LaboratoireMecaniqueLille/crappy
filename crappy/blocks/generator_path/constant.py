b3d1de9dc8b83608667ebe05ee5991ba07a1e28a
Victor Couty <victor@couty.eu>
2017-09-27 14:26:24 +0200

    [add] Option to make constant path send at least one value

--- crappy/blocks/generator_path/constant.py
+++ crappy/blocks/generator_path/constant.py
@@ -12,13 +12,24 @@ class Constant(Path):
 
     condition: String representing the condition to end this path.
     See Path.parse_condition for more detail.
+
+    send_one: If True, this condition will send the value at least once
+    before checking the condition
   """
-  def __init__(self,time,cmd,condition,value=None):
+  def __init__(self,time,cmd,condition,send_one=True,value=None):
     Path.__init__(self,time,cmd)
     self.condition = self.parse_condition(condition)
     self.value = cmd if value is None else value
+    if send_one:
+      self.get_cmd = self.get_cmd_first
+    else:
+      self.get_cmd = self.get_cmd_condition
 
-  def get_cmd(self,data):
+  def get_cmd_first(self,data):
+    self.get_cmd = self.get_cmd_condition
+    return self.value
+
+  def get_cmd_condition(self,data):
     if self.condition(data):
       raise StopIteration
     return self.value
