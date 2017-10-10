dd2fea8966c88706bd5c7743f55894d6469f1236
Victor Couty <victor@couty.eu>
2017-10-10 10:02:06 +0200

    [add] Support for dio in nidaqmx

---
+++ Examples/dio_daqmx.py
@@ -0,0 +1,25 @@
+#coding: utf-8
+
+import crappy
+
+
+def intify(data):
+  for i,d in enumerate(data):
+    if isinstance(d,bool):
+      data[i] = int(d)
+  return data
+
+if __name__ == "__main__":
+  gen = crappy.blocks.Generator([
+  dict(type='cyclic',value1=0,value2=1,condition1="delay=1",condition2="delay=1")
+  ],repeat=True)
+  io = crappy.blocks.IOBlock("Nidaqmx",device="Dev2",
+      channels=[dict(name='ai0'),dict(name='di0'),dict(name='ao0'),dict(name='do1')],
+      samplerate = 100,
+      labels = ['t(s)','ai0','di0'],
+      cmd_labels = ['cmd','cmd'],
+      )
+  crappy.link(gen,io)
+  graph = crappy.blocks.Grapher(('t(s)','di0'),('t(s)','ai0'))
+  crappy.link(io,graph,condition=intify)
+  crappy.start()
--- crappy/inout/nidaqmx.py
+++ crappy/inout/nidaqmx.py
@@ -45,6 +45,8 @@ class Nidaqmx(InOut):
     self.streaming = False
     self.ao_channels = []
     self.ai_channels = {}
+    self.di_channels = []
+    self.do_channels = []
     for c in self.channels:
       c['type'] = c.get('type','voltage').lower()
       if c['name'].startswith('ai'):
@@ -54,16 +56,21 @@ class Nidaqmx(InOut):
           self.ai_channels[c['type']] = [c]
       elif c['name'].startswith('ao'):
         self.ao_channels.append(c)
+      elif c['name'].startswith('di'):
+        c['name'] = 'line'+c['name'][2:]
+        self.di_channels.append(c)
+      elif c['name'].startswith('do'):
+        c['name'] = 'line'+c['name'][2:]
+        self.do_channels.append(c)
       else:
         raise AttributeError("Unknown channel in nidaqmx"+str(c))
     self.ai_chan_list = sum(self.ai_channels.values(),[])
-    
+
 
   def open(self):
+    # AI
     self.t_in = {}
-    if self.ao_channels:
-      self.t_out = nidaqmx.Task()
-    
+
     for c in self.ai_chan_list:
       kwargs = dict(c)
       kwargs.pop("name",None)
@@ -83,6 +90,15 @@ class Nidaqmx(InOut):
       except Exception as e:
         print("Invalid channel settings in nidaqmx:"+str(c))
         raise
+    self.stream_in = {}
+    for chan_type,t in self.t_in.items():
+      self.stream_in[chan_type] = \
+          stream_readers.AnalogMultiChannelReader(t.in_stream)
+    # AO
+    if self.ao_channels:
+      self.t_out = nidaqmx.Task()
+      self.stream_out = stream_writers.AnalogMultiChannelWriter(
+          self.t_out.out_stream,auto_start=True)
 
     for c in self.ao_channels:
       kwargs = dict(c)
@@ -90,18 +106,36 @@ class Nidaqmx(InOut):
       kwargs.pop("type",None)
       self.t_out.ao_channels.add_ao_voltage_chan(
           "/".join([self.device,c['name']]),min_val=0,max_val=5, **kwargs)
+    # DI
+    if self.di_channels:
+      self.t_di = nidaqmx.Task()
+    for c in self.di_channels:
+      kwargs = dict(c)
+      kwargs.pop("name",None)
+      kwargs.pop("type",None)
+      self.t_di.di_channels.add_di_chan(
+          "/".join([self.device,c['name']]), **kwargs)
+    if self.di_channels:
+      self.di_stream = stream_readers.DigitalMultiChannelReader(
+          self.t_di.in_stream)
 
-    self.stream_in = {}
-    for chan_type,t in self.t_in.items():
-      self.stream_in[chan_type] = \
-          stream_readers.AnalogMultiChannelReader(t.in_stream)
-    if self.ao_channels:
-      self.stream_out = stream_writers.AnalogMultiChannelWriter(
-          self.t_out.out_stream,auto_start=True)
-    print("DEBUG",self.ai_channels)
+    # DO
+    if self.do_channels:
+      self.t_do = nidaqmx.Task()
+    for c in self.do_channels:
+      kwargs = dict(c)
+      kwargs.pop("name",None)
+      kwargs.pop("type",None)
+      self.t_do.do_channels.add_do_chan(
+          "/".join([self.device,c['name']]), **kwargs)
+    if self.do_channels:
+      self.do_stream = stream_writers.DigitalMultiChannelWriter(
+          self.t_do.out_stream)
 
   def start_stream(self):
-    for t in self.t_in.values():
+    if len(self.t_in) != 1:
+      raise IOError("Stream mode can only open one type of chanchan type in stream mode!")
+    for t in self.t_in.values(): # Only one loop
       t.timing.cfg_samp_clk_timing(self.samplerate,
           sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
     self.streaming = True
@@ -117,10 +151,8 @@ class Nidaqmx(InOut):
     if not self.streaming:
       self.start_stream()
     a = np.empty((len(self.ai_chan_list),self.nsamples))
-    i = 0
-    for chan_type,s in self.stream_in.items():
-      s.read_many_sample(a[i:i+len(self.ai_channels[chan_type]),:],self.nsamples)
-      i += len(self.ai_channels[chan_type])
+    for chan_type,s in self.stream_in.items(): #Will only loop once
+      s.read_many_sample(a,self.nsamples)
     return [time(),a]
 
   def close(self):
@@ -134,15 +166,24 @@ class Nidaqmx(InOut):
       t.close()
     if self.ao_channels:
       self.t_out.close()
-    
+
   def get_data(self):
-    a = np.empty((len(self.ai_channels)))
+    a = np.empty((len(self.ai_channels),))
     i = 0
     t = time()
     for chan_type,s in self.stream_in.items():
       s.read_one_sample(a[i:i+len(self.ai_channels[chan_type])])
       i += len(self.ai_channels[chan_type])
+    if self.di_channels:
+      b = np.empty((len(self.di_channels),1),dtype=np.bool)
+      self.di_stream.read_one_sample_multi_line(b)
+      return [t]+list(a)+list(b[:,0])
     return [t]+list(a)
 
-  def set_cmd(self,v):
-    self.stream_out.write_one_sample(np.array([v],dtype=np.float64))
+  def set_cmd(self,*v):
+    if self.ao_channels:
+      self.stream_out.write_one_sample(np.array(v[:len(self.ao_channels)],
+        dtype=np.float64))
+    if self.do_channels:
+      self.do_stream.write_one_sample_multi_line(
+        np.array(v[len(self.ao_channels):],dtype=np.bool).reshape(len(self.do_channels),1))
