0e4da5f2bfaa26b3e526f41cdab8d3f57cc8e90c
Victor Couty <victor@couty.eu>
2019-01-11 15:47:16 +0100

    [add] Correl block now saves initial image

--- crappy/blocks/correl.py
+++ crappy/blocks/correl.py
@@ -24,7 +24,7 @@ class Correl(MasterBlock):
         .start() method is called (after dropping the first image).
   """
 
-  def __init__(self, camera="Ximea", **kwargs):
+  def __init__(self, camera, **kwargs):
     MasterBlock.__init__(self)
     self.ready = False
     self.camera_name = camera
@@ -98,6 +98,10 @@ with fields=(.,.) or Nfields=k")
     self.correl.setOrig(img.astype(np.float32))
     self.correl.prepare()
     self.last_t = time() - 1
+    if self.save_folder:
+      image = sitk.GetImageFromArray(img)
+      sitk.WriteImage(image,
+               self.save_folder + "img_ref_%.5f.tiff" % ( t-self.t0))
 
   def loop(self):
     if self.verbose and self.loops%self.nloops == 0:
