1b2d1df82ca1dcf86b0498184b771243f634949e
Victor Couty <victor@couty.eu>
2019-04-09 11:12:18 +0200

    [add] Can now resize displayer when using tk

--- crappy/blocks/displayer.py
+++ crappy/blocks/displayer.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from .masterblock import MasterBlock
+from .._global import CrappyStop
 import cv2
 import matplotlib.pyplot as plt
 import tkinter as tk
@@ -88,15 +89,36 @@ class Displayer(MasterBlock):
   def resize(self,img):
     return cv2.resize(img,(self.w,self.h))
 
+  def check_resized(self):
+    new = self.imglabel.winfo_height()-2,self.imglabel.winfo_width()-2
+    if sum([abs(i-j) for i,j in zip(new,(self.h,self.w))]) >= 5:
+      if new[0] > 0 and new[1] > 0:
+        self.h,self.w = new
+        ratio = min(self.h/self.img_shape[0],
+                self.w/self.img_shape[1])
+        self.h = int(self.img_shape[0]*ratio)
+        self.w = int(self.img_shape[1]*ratio)
+
   def begin_tk(self):
     self.root = tk.Tk()
+    self.root.protocol("WM_DELETE_WINDOW",self.end)
     self.imglabel = tk.Label(self.root)
     self.imglabel.pack()
+    #self.imglabel.grid(row=0,column=0,sticky=tk.N+tk.E+tk.S+tk.W)
+    self.imglabel.pack(expand=1,fill=tk.BOTH)
     self.h = 480
     self.w = 640
+    data = self.inputs[0].recv_delay(self.delay)['frame'][-1]
+    self.img_shape = data.shape
+    self.check_resized()
+    self.go = True
 
   def loop_tk(self):
+    if not self.go:
+      raise CrappyStop
     data = self.inputs[0].recv_delay(self.delay)['frame'][-1]
+    self.img_shape = data.shape
+    self.check_resized()
     if data.dtype != np.uint8:
       if data.max() >= 256:
         data = self.cast_8bits(data)
@@ -105,6 +127,9 @@ class Displayer(MasterBlock):
     cimg = ImageTk.PhotoImage(Image.fromarray(self.resize(data)))
     self.imglabel.configure(image=cimg)
     self.root.update()
+
+  def end(self):
+    self.go = False
 
   def finish_tk(self):
     self.root.destroy()
