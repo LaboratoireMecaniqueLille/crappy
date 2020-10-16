b2791d08f9a40dfbe19a4f3eb0c2ccefe0762dec
Victor Couty <victor@couty.eu>
2020-10-16 14:29:03 +0200

    [add] Selectable backend for saving images

--- crappy/blocks/camera.py
+++ crappy/blocks/camera.py
@@ -1,22 +1,36 @@
 # coding: utf-8
 
+from sys import platform
 import os
 import time
 try:
   import SimpleITK as sitk
 except ImportError:
   sitk = None
+import cv2
 
 from .masterblock import MasterBlock
 from ..camera import camera_list
 from ..tool import Camera_config
+
+kw = dict(
+  ("save_folder",None),
+  ("verbose",False),
+  ("labels",['t(s)','frame']),
+  ("fps_label",False),
+  ("img_name","{self.loops:06d}_{t-self.t0:.6f}"),
+  ("ext","tiff"),
+  ("save_period",1),
+  ("save_backend",None),
+  ("config",True)
+)
 
 
 class Camera(MasterBlock):
   """
   Streams pictures.
   """
-  def __init__(self,camera="Fake_camera",**kwargs):
+  def __init__(self,camera,**kwargs):
     """
     Read images from a camera object, save and/or send them to another block.
 
@@ -26,23 +40,32 @@ class Camera(MasterBlock):
     kwargs:
       camera : {"Ximea","Jai","Webcam",...}
         See crappy.camera.MasterCam.classes for a full list
-        (str, default="Fake_camera")
+        (str, mandatory)
       save_folder : directory to save the images. It will be created
         if necessary. If None, it will not save the images
         (str or None, default: None)
       verbose : If True, the block will print the number of fps
-          (bool, default=False)
-      labels : string, default=['t(s)','frame']
-        The labels for respectively time and the frame
-      config : Show the popup for config ? (bool, default = True)
+          (bool, default: False)
+      labels : The labels for respectively time and the frame
+          (list of strings, default: ['t(s)','frame'])
+      fps_label : If set, self.max_fps will be set to the value received by
+        the block with this label
+      img_name : Template for the name of the image to save
+        it will be evaluated as a f-string
+        (string, default: "{self.loops:06d}_{t-self.t0:.6f}"
+      ext : Extension of the image. Make sure it is supported by the
+        image saving backend (str,default: "tiff")
+      save_period : Will save only one in x images
+        (int, default : 1)
+      save_backend : module to use to save the images
+        supported backends : sitk, cv2
+        if None will try sitk, else cv2
+        (str, default: None)
+      config : Show the popup for config ? (bool, default: True)
     """
     MasterBlock.__init__(self)
     self.niceness = -10
-    for arg,default in [("save_folder",None),
-                        ("verbose",False),
-                        ("labels",['t(s)','frame']),
-                        ("fps_label",""),
-                        ("config",True)]:
+    for arg,default in kw.items():
       setattr(self,arg,kwargs.get(arg,default)) # Assign these attributes
       try:
         del kwargs[arg]
@@ -54,8 +77,19 @@ class Camera(MasterBlock):
     self.cam_kw = kwargs
     assert self.camera_name in camera_list,"{} camera does not exist!".format(
         self.camera_name)
+    if self.save_backend is None:
+      if sitk is None:
+        self.save_backend = "cv2"
+      else:
+        self.save_backend = "sitk"
+    assert self.save_backend in ["cv2","sitk"],\
+        "Unknown saving backend: "+self.save_backend
+    self.save = getattr(self,"save_"+self.save_backend)
 
   def prepare(self):
+    sep = '\\' if 'win' in platform else '/'
+    if self.save_folder and not self.save_folder.endswith(sep):
+      self.save_folder += sep
     if self.save_folder and not os.path.exists(self.save_folder):
       try:
         os.makedirs(self.save_folder)
@@ -64,9 +98,7 @@ class Camera(MasterBlock):
             "Error creating "+self.save_folder
     self.camera = camera_list[self.camera_name]()
     self.camera.open(**self.cam_kw)
-    #self.trigger = "internal" if len(self.inputs) == 0 else "external"
-    self.trigger = "external" if self.inputs and self.fps_label == ""\
-        else "internal"
+    self.ext_trigger = True if self.inputs and not self.fps_label else False
     if self.config:
       conf = Camera_config(self.camera)
       conf.main()
@@ -78,25 +110,28 @@ class Camera(MasterBlock):
     self.last_index = 0
     self.loops = 0
 
+  def save_sitk(self,img,fname):
+    image = sitk.GetImageFromArray(img)
+    sitk.WriteImage(image,fname)
+
+  def save_cv2(self,img,fname):
+    cv2.imwrite(fname,img)
+
   def loop(self):
-    if self.trigger == "internal":
+    if not self.ext_trigger:
       if self.fps_label:
         while self.inputs[0].poll():
           self.camera.max_fps = self.inputs[0].recv()[self.fps_label]
       t,img = self.camera.read_image()
-    elif self.trigger == "external":
+    else:
       data = self.inputs[0].recv()  # wait for a signal
       if data is None:
         return
       t,img = self.camera.get_image()
     self.timer = time.time()
-    if self.save_folder:
-      if not sitk:
-        raise IOError("[Camera] Cannot save image, sitk is not installed !")
-      image = sitk.GetImageFromArray(img)
-      sitk.WriteImage(image,
-               self.save_folder + "img_%.6d_%.5f.tiff" % (
-               self.loops, t-self.t0))
+    if self.save_folder and self.loops % self.save_period == 0:
+      self.save(img, self.save_folder +
+          eval('f"{}"'.format(self.img_name)) + f".{self.ext}")
     self.loops += 1
     self.send([t-self.t0,img])
 
