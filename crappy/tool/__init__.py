6e424508420e94df44addb4cdecbcd5edb358e0b
Victor Couty <victor@couty.eu>
2021-01-15 12:09:10 +0100

    [del] removed unused DataPicker

--- crappy/tool/__init__.py
+++ crappy/tool/__init__.py
@@ -1,8 +1,8 @@
 #coding: utf-8
+
 from .._global import NotInstalled
 from .cameraConfig import Camera_config
 from .videoextensoConfig import VE_config
-from .datapicker import DataPicker
 try:
   from .gpucorrel import GPUCorrel
 except ImportError:
