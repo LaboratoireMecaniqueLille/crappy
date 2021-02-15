6f2e23bdf398813a32586b55a5bd800608a5de3f
Victor Couty <victor@couty.eu>
2021-02-15 16:23:19 +0100

    [ref] Updated Examples to use modifier instead of condition (forgot two)

--- Examples/test_graph.py
+++ Examples/test_graph.py
@@ -3,6 +3,6 @@ import crappy
 gen = crappy.blocks.Generator([{'type':'ramp','condition':None,
   'speed':1}],freq=500)
 graph = crappy.blocks.Grapher(('t(s)','i_cmd'),maxpt=10,interp=True)
-crappy.link(gen,graph,condition=crappy.condition.Integrate('cmd'))
+crappy.link(gen,graph,modifier=crappy.modifier.Integrate('cmd'))
 
 crappy.start()
