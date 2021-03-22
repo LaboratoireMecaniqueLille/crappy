6e5931ba94680f2da5075bb478a771cf72d928fa
Victor Couty <victor@couty.eu>
2021-03-22 14:14:20 +0100

    [ref] Optimized apply_strain modifier

--- crappy/modifier/apply_strain_img.py
+++ crappy/modifier/apply_strain_img.py
@@ -26,11 +26,12 @@ class Apply_strain_img(Modifier):
           dtype=np.float32)[np.newaxis, :],)*h, axis=0)
     self.eyy = np.concatenate((np.linspace(-h/2, h/2, h,
       dtype=np.float32)[:, np.newaxis],)*w, axis=1)
-    self.xx,self.yy = np.meshgrid(range(w),range(h))
+    xx,yy = np.meshgrid(range(w),range(h))
+    self.xx = xx.astype(np.float32)
+    self.yy = yy.astype(np.float32)
 
   def evaluate(self,d):
     exx,eyy = d[self.lexx]/100,d[self.leyy]/100
-    d[self.img_label] = cv2.remap(self.img,
-      (self.xx-(exx/(1+exx))*self.exx).astype(np.float32),
-      (self.yy-(eyy/(1+eyy))*self.eyy).astype(np.float32),1)
+    tx,ty = (self.xx-(exx/(1+exx))*self.exx),(self.yy-(eyy/(1+eyy))*self.eyy)
+    d[self.img_label] = cv2.remap(self.img,tx,ty,1)
     return d
