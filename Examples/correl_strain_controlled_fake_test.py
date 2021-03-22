923c985749eedf8745f829be75388f4464e333dd
Victor Couty <victor@couty.eu>
2021-03-22 14:16:57 +0100

    [fix] Renamed setpoint label to target_Exx for clarity, tweaked performance

--- Examples/correl_strain_controlled_fake_test.py
+++ Examples/correl_strain_controlled_fake_test.py
@@ -18,31 +18,32 @@ speed = .05 # Strain rate (%/s)
 # Simply create a sawtooth signal at 0.05 units/s between 0 and 1
 # Note that this generator takes no feedback
 generator = crappy.blocks.Generator(path=[
-  {'type':'cyclic_ramp','speed1':speed,'condition1':'setpoint>1',
-    'speed2':-speed,'condition2':'setpoint<0','cycles':100},
-    ],cmd_label='setpoint')
+  {'type':'cyclic_ramp','speed1':speed,'condition1':'target_Exx(%)>1',
+    'speed2':-speed,'condition2':'target_Exx(%)<0','cycles':100},
+    ],cmd_label='target_Exx(%)')
 
 # Our fake machine
-machine = crappy.blocks.Fake_machine(maxstrain=17,k=5000,l0=20,
+machine = crappy.blocks.Fake_machine(maxstrain=1.7,k=5000,l0=20,
     plastic_law=lambda exx: 0,sigma={'F(N)':0.5},
     cmd_label='pid')
 
 # The block performing the DIC
 dis = crappy.blocks.DISCorrel('',input_label='frame',show_image=True,
-    labels=['t(s)','x','y','measured_Exx(%)','measured_Eyy(%)'],verbose=True)
+    labels=['t(s)','x','y','measured_Exx(%)','measured_Eyy(%)'],verbose=True,
+    iterations=0,finest_scale=2)
 
 # This modifier will generate an image with the values of strain
 # coming from the Fake_machine block
 crappy.link(machine,dis,modifier=crappy.modifier.Apply_strain_img(img))
 
 # The PID block takes TWO inputs: the setpoint and the feedback
-# Here the setpoint is coming from the generator
-# and the feedback is the strain measured with the DIC
+# Here the setpoint is coming from the generator (target_Exx(%))
+# and the feedback is the strain measured with the DIC (measured_Exx(%))
 # The output is the command sent to the machine
 # In a real-world scenario, consider using out_min and out_max
 # to clamp the output value and i_limit to prevent over_integration.
 pid = crappy.blocks.PID(kp=0.5,ki=2,kd=0.05,
-    target_label='setpoint', input_label='measured_Exx(%)', send_terms=True)
+    target_label='target_Exx(%)', input_label='measured_Exx(%)', send_terms=True)
 
 # We link the two inputs and the output
 # The order does not matter, the inputs are identified thanks to the
@@ -60,7 +61,7 @@ crappy.link(pid,graph_pid)
 
 # To see the commanded and the measured strains
 graph_def2 = crappy.blocks.Grapher(('t(s)','measured_Exx(%)'),
-                                   ('t(s)','setpoint'))
+                                   ('t(s)','target_Exx(%)'))
 crappy.link(dis,graph_def2)
 crappy.link(generator,graph_def2)
 
