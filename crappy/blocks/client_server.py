4b314653d226c75813a88d3b3f8b7aae8fc8faf5
Antoine <antoine.weisrock@gmail.com>
2021-05-19 11:07:19 +0200

    [upd] Updated docmentation for the client_server block

--- crappy/blocks/client_server.py
+++ crappy/blocks/client_server.py
@@ -2,6 +2,7 @@
 
 from .block import Block
 from .._global import OptionalModule
+from typing import Dict, List, Union, Tuple, Any
 import time
 import subprocess
 from threading import Thread
@@ -16,106 +17,136 @@ except (ModuleNotFoundError, ImportError):
 
 
 class Client_server(Block):
+  """Block for exchanging data on a local network using the MQTT protocol
+
+  This block can send data to an MQTT broker, receive data from this broker by
+  subscribing to its topics, and also launch the Mosquitto broker.
   """
-  The Client_server block is meant for exchanging data with distant devices
-  on a local network, using the MQTT protocol.
 
-  Args:
-    - broker=False (bool, default: False, optional): If True, starts the
-      Mosquitto broker during the prepare loop and stops it during the
-      finish loop. If Mosquitto is not installed a FileNotFoundError will
-      be raised.
+  def __init__(self,
+               broker: bool = False,
+               address: Any = 'localhost',
+               port: int = 1148,
+               init_output: Dict[str, Any] = None,
+               topics: List[Union[str, Tuple[str]]] = None,
+               cmd_labels: List[Union[str, Tuple[str]]] = None,
+               labels_to_send: List[Union[str, Tuple[str]]] = None) -> None:
+    """Checks arguments validity and sets the instance attributes
 
-        Note:
-          An MQTT broker must be running at the specified address on the
-          specified port. If not, a ConnectionRefusedError will be raised.
-          The broker can be started and stopped manually by the user
-          independently of the execution of crappy. It also doesn't need to be
-          Mosquitto, any other MQTT broker can be used.
+    Note:
+      - ``broker``:
+        In order for the block to run, an MQTT broker must be running at the
+        specified address on the specified port. If not, an
+        :obj:`ConnectionRefusedError` is raised. The broker can be started and
+        stopped manually by the user independently of the execution of crappy.
+        It also doesn't need to be Mosquitto, any other MQTT broker can be
+        used.
 
-    - address='localhost' (str, default: 'localhost', optional): The network
-      address on which the MQTT broker is running.
+      - ``topics``:
+        The presence of the same label in multiple topics will most likely
+        lead to a data loss.
 
-    - port=1148 (int, default: 1148, optional): A network port on which the
-      MQTT broker is listening.
+      - ``cmd_labels``:
+        It is not possible to group signals coming from different blocks in a
+        same topic.
 
-    - init_output=None (dict, default: None, optional): A dictionary
-      containing for each label in topics the first value to be sent in the
-      output link.
+      - ``labels_to_send``:
+        Differences in the structure of ``labels_to_send`` and ``cmd_labels``
+        will not always raise an error, but may lead to a data loss.
 
-    - topics=None (list, default: None, optional): A list of strings and/or
-      tuples of strings. Each string corresponds to the name of a crappy label
-      to be received from the broker. Each element of the list is considered to
-      be the name of an MQTT topic, to which the client subscribes. After a
-      message has been received on that topic, the block returns for each label
-      in the topic (i.e. each string in the tuple) the corresponding data from
-      the message.
+      - **Single-value tuples**:
+        Single-value tuples can be shortened as strings.
+        ::
+          topics=[('cmd1',), ('cmd2',)]
+          cmd_labels=[('cmd1',), ('cmd2',)]
+          labels_to_send=[('cmd1',), ('cmd2',)]
+        is equivalent to
+        ::
+          topics=['cmd1', 'cmd2']
+          cmd_labels=['cmd1', 'cmd2']
+          labels_to_send=['cmd1', 'cmd2']
 
-        Note:
-          Single-value tuples can be shortened as strings.
-          topics=[('cmd1',), ('cmd2',)] is equivalent to topics=['cmd1',
-          'cmd2'].
+    Examples:
+      - ``topics``:
+        If
+        ::
+          topics=[('t1', 'cmd1'), 'sign']
+        the client will subscribe to the topics
+        ::
+          ('t1', 'cmd1')
+          ('sign',)
+        The block will return data associated with the labels
+        ::
+          't1', 'cmd1'
+          'sign'
 
-        Note:
-          The presence of the same label in multiple topics will most likely
-          lead to a data loss.
+      - ``cmd_labels``:
+        If
+        ::
+          cmd_labels=[('t1', 'cmd1'), 'sign']
+        the client will publish data in the form of
+        ::
+          [[t1_0, cmd1_0], [t1_1, cmd1_1], ...]
+          [[sign_0], [sign_1], ...]
+        in the topics
+        ::
+           ('t1', 'cmd1')
+           ('sign',)
 
-        Example:
-          If topics=[('t1', 'cmd1'), 'sign'], the client will subscribe to the
-          topics ('t1', 'cmd1') and ('sign',).
-          The block will return data associated with the labels 't1', 'cmd1'
-          and 'sign'.
+      - ``labels_to_send``:
+        If
+        ::
+          cmd_labels=[('t(s)', 'cmd'), 'sign']
+          labels_to_send=[('t1', 'cmd1'), 'sign']
+        the data from labels
+        ::
+          't(s)', 'cmd'
+        will be published in the topic
+        ::
+          ('t1', 'cmd1')
+        and the data from label
+        ::
+          'sign'
+        in the topic
+        ::
+          ('sign',)
 
-    - cmd_labels=None (list, default: None, optional): A list of strings and/or
-      tuples of strings. Each string corresponds to the name of a crappy label
-      to be sent to the broker. Each element of the list is considered to be
-      the name of an MQTT topic, in which the client publishes. Grouping labels
-      in a same topic (i.e. strings in a same tuple) allows to keep the
-      synchronization between signals coming from a same block, as they will be
-      published together in a same message. This is mostly useful for sending a
-      signal along with its timeframe.
-
-        Note:
-          It is not possible to group signals coming from different blocks in a
-          same topic.
-
-        Note:
-          Single-value tuples can be shortened as strings.
-          cmd_labels=[('cmd1',), ('cmd2',)] is equivalent to
-          cmd_labels=['cmd1', 'cmd2'].
-
-        Example:
-          If cmd_labels=[('t1', 'cmd1'), 'sign'], the client will publish data
-          in the form of [[t1_0, cmd1_0], [t1_1, cmd1_1], ...] in the topic
-          ('t1', 'cmd1') and [[sign_0], [sign_1], ...] in the topic ('sign',).
-
-    - labels_to_send=None (list, default: None, optional): A list of strings
-      and/or tuples of strings. Allows to rename the labels before publishing
-      data. The structure of labels_to_send should be the exact same as
-      cmd_labels, with each label in labels_to_send replacing the corresponding
-      one in cmd_labels. This is especially useful for transferring several
-      signals along with their timestamps, as the label 't(s)' should not
-      appear more than once in the topics list of the receiving block.
-
-        Note:
-          Single-value tuples can be shortened as strings.
-          labels_to_send=[('cmd1',), ('cmd2',)] is equivalent to
-          labels_to_send=['cmd1', 'cmd2'].
-
-        Note:
-          Differences in the structure of labels_to_send and cmd_labels
-          will not always raise an error, but may lead to a data loss.
-
-        Example:
-          If cmd_labels=[('t(s)', 'cmd'), 'sign'] and
-          labels_to_send=[('t1', 'cmd1'), 'sign'], the data from labels
-          't(s)' and 'cmd' will be published in the topic ('t1', 'cmd1')
-          and the data from label 'sign' in the topic ('sign',).
+    Args:
+      broker (:obj:`bool`, optional): If True, starts the Mosquitto broker
+         during the prepare loop and stops it during the finish loop. If
+         Mosquitto is not installed a :obj:`FileNotFoundError` is raised.
+      address (optional): The network address on which the MQTT broker is
+        running.
+      port (:obj:`int`, optional): A network port on which the MQTT broker is
+        listening.
+      init_output (:obj:`dict`, optional): A dictionary containing for each
+        label in ``topics`` the first value to be sent in the output link. Must
+        be given if ``topics`` is not None.
+      topics (:obj:`list`, optional): A list of strings and/or tuples of
+        strings. Each string corresponds to the name of a crappy label to be
+        received from the broker. Each element of the list is considered to be
+        the name of an MQTT topic, to which the client subscribes. After a
+        message has been received on that topic, the block returns for each
+        label in the topic (i.e. each string in the tuple) the corresponding
+        data from the message.
+      cmd_labels (:obj:`list`, optional): A list of strings and/or tuples of
+        strings. Each string corresponds to the name of a crappy label to send
+        to the broker. Each element of the list is considered to be the name of
+        an MQTT topic, in which the client publishes. Grouping labels in a same
+        topic (i.e. strings in a same tuple) allows to keep the synchronization
+        between signals coming from a same block, as they will be published
+        together in a same message. This is mostly useful for sending a signal
+        along with its timeframe.
+      labels_to_send (:obj:`list`, optional): A list of strings and/or tuples
+        of strings. Allows to rename the labels before publishing data. The
+        structure of ``labels_to_send`` should be the exact same as
+        ``cmd_labels``, with each label in ``labels_to_send`` replacing the
+        corresponding one in ``cmd_labels``. This is especially useful for
+        transferring several signals along with their timestamps, as the label
+        ``'t(s)'`` should not appear more than once in the topics list of the
+        receiving block.
     """
 
-  def __init__(self, broker=False, address='localhost', port=1148,
-               init_output=None, topics=None, cmd_labels=None,
-               labels_to_send=None):
     Block.__init__(self)
     self.niceness = -10
     self.stop_mosquitto = False
@@ -206,12 +237,18 @@ class Client_server(Block):
 
     self.client.loop_start()
 
-  def loop(self):
-    # Loop for receiving data
-    # Each queue in the buffer is checked once:
-    # if not empty then the first list of data is popped.
-    # This data is then associated with the corresponding labels in dict_out.
-    # dict_out is finally returned if not empty
+  def loop(self) -> None:
+    """Receives data from the broker and/or sends data to the broker
+
+    The received data is then sent to the crappy blocks connected to this one.
+    """
+
+    """Loop for receiving data
+    Each queue in the buffer is checked once: if not empty then the first list 
+    of data is popped. This data is then associated to the corresponding 
+    labels in dict_out. dict_out is finally returned if not empty. All the 
+    labels should be returned at each loop iteration, so a buffer stores the 
+    last value for each label and returns it if no other value was received."""
     if self.topics is not None:
       dict_out = {}
       for topic in self.buffer_output:
@@ -233,12 +270,11 @@ class Client_server(Block):
               self.last_out_val[label] = dict_out[label]
         self.send(dict_out)
 
-    # Loop for sending data
-    # Data is first received as a list of dictionaries
-    # For each topic, trying to find a dictionary containing all the
-    # corresponding labels
-    # Once this dictionary has been found, its data is published as a list of
-    # list of values
+    """Loop for sending data
+    Data is first received as a list of dictionaries. For each topic, trying to 
+    find a dictionary containing all the corresponding labels. Once this 
+    dictionary has been found, its data is published as a list of list of 
+    values."""
     if self.cmd_labels is not None:
       received_data = [link.recv_chunk() if link.poll() else {} for link
                        in self.inputs]
@@ -258,7 +294,9 @@ class Client_server(Block):
                                   qos=0)
             break
 
-  def finish(self):
+  def finish(self) -> None:
+    """Disconnects from the broker and stops it"""
+
     # Disconnecting from the broker
     self.client.loop_stop()
     self.client.disconnect()
@@ -275,7 +313,9 @@ class Client_server(Block):
         print('Subprocess did not terminate in time')
         self.proc.kill()
 
-  def _launch_mosquitto(self):
+  def _launch_mosquitto(self) -> None:
+    """Starts Mosquitto in a subprocess"""
+
     try:
       self.proc = subprocess.Popen(['mosquitto', '-p', str(self.port)],
                                    stdout=subprocess.PIPE,
@@ -284,7 +324,9 @@ class Client_server(Block):
       print("Mosquitto is not installed !")
       raise
 
-  def _output_reader(self):
+  def _output_reader(self) -> None:
+    """Reads the output strings from Mosquitto's subprocess"""
+
     while not self.stop_mosquitto:
       for line in iter(self.proc.stdout.readline, b''):
         print('[Mosquitto] {0}'.format(line.decode('utf-8')), end='')
@@ -292,15 +334,23 @@ class Client_server(Block):
           print('Mosquitto is already running on this port')
       time.sleep(0.1)
 
-  def _on_message(self, client, userdata, message):
-    # The received message consists in a list of lists of values
-    # Data is placed in the right buffer according to the topic, in the
-    # form of lists of values
+  def _on_message(self,
+                  client,
+                  userdata,
+                  message: mqtt.MQTTMessage) -> None:
+    """Buffers the received data
+
+    The received message consists in a list of lists of values. Data is placed
+    in the right buffer according to the topic, in the form of lists of values.
+    """
+
     for data_points in zip(*pickle.loads(message.payload)):
       self.buffer_output[ast.literal_eval(message.topic)].put_nowait(
         list(data_points))
 
-  def _on_connect(self, client, userdata, flags, rc):
+  def _on_connect(self, client, userdata, flags, rc: Any):
+    """Automatically subscribes to the topics when connecting to the broker"""
+
     print("Connected with result code " + str(rc))
 
     # Subscribing on connect, so that it automatically resubscribes when
