c4074cb36306edeb53a81c98bae3ea48b90ec18a
Antoine <antoine.weisrock@gmail.com>
2021-05-27 16:45:35 +0200

    [ref] Making crappy/_global.py PEP-8 compliant

--- crappy/_global.py
+++ crappy/_global.py
@@ -1,5 +1,4 @@
-#coding:utf-8
-
+# coding:utf-8
 
 class OptionalModule:
   """
@@ -7,15 +6,16 @@ class OptionalModule:
 
   Will print a message and raise an error when trying to use them
   """
-  def __init__(self,module_name,message=None):
+
+  def __init__(self, module_name, message=None):
     self.mname = module_name
     if message is not None:
       self.message = message
     else:
-      self.message = """The module {} is necessary to use this fucntionnality
+      self.message = """The module {} is necessary to use this functionality
 Please install it and try again""".format(self.mname)
 
-  def __getattr__(self,arg):
+  def __getattr__(self, arg):
     print("Missing module: {}".format(self.mname))
     print(self.message)
     raise RuntimeError(self.message)
@@ -23,12 +23,14 @@ Please install it and try again""".format(self.mname)
 
 class CrappyStop(Exception):
   """Error to raise when Crappy is terminating"""
+
   pass
 
 
 class DefinitionError(Exception):
     """Error to raise when classes are not defined correctly"""
-    def __init__(self,msg=""):
+
+    def __init__(self, msg=""):
         self.msg = msg
 
     def __str__(self):
