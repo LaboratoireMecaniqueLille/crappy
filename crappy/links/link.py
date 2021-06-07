7a1e39693c4cc868221f4dbfd8def3ef87c00304
Antoine <antoine.weisrock@gmail.com>
2021-06-07 19:53:16 +0200

    [ref] Documentation update for the links
    Also added typehints

--- crappy/links/link.py
+++ crappy/links/link.py
@@ -7,6 +7,8 @@ from multiprocessing import Pipe
 from time import time
 from threading import Thread
 from copy import copy
+from functools import wraps
+from typing import Callable, Any, Union
 
 from .._global import CrappyStop
 
@@ -14,9 +16,10 @@ from .._global import CrappyStop
 def error_if_string(recv: Callable) -> Callable:
   """Decorator to raise an error if the function returns a string."""
 
+  @wraps(recv)
   def wrapper(*args, **kwargs):
     ret = recv(*args, **kwargs)
-    if type(ret) == str:
+    if isinstance(ret, str):
       raise CrappyStop
     return ret
   return wrapper
@@ -29,9 +32,10 @@ class TimeoutError(Exception):
 
 
 class MethodThread(Thread):
-  """
-  ThreadMethodThread, daemonic descendant class of threading. Thread which
-  simply runs the specified target method with the specified arguments.
+  """ThreadMethodThread, daemonic descendant class of :mod:`threading`.
+
+  Thread which simply runs the specified target method with the specified
+  arguments.
   """
 
   def __init__(self, target, args, kwargs):
@@ -49,10 +53,11 @@ class MethodThread(Thread):
       self.exception = None
 
 
-def win_timeout(timeout=None):
+def win_timeout(timeout: float = None):
   """Decorator for adding a timeout to a link send."""
 
-  def win_timeout_proxy(f):
+  def win_timeout_proxy(f: Callable) -> Callable:
+    @wraps(f)
     def wrapper(*args, **kwargs):
       worker = MethodThread(f, args, kwargs)
       if timeout is None:
@@ -71,39 +76,42 @@ def win_timeout(timeout=None):
 
 
 class Link(object):
-  """
-  Link class. All connection between Blocks should be made with this.
+  """Link class. All connections between Blocks should be made with this.
+
   It creates a pipe and is used to transfer information between Blocks.
 
   Note:
-    You can add one or multiple conditions to modify the value transferred.
-    Creates a pipe and is used to transfer information between Blocks.
-    You can add one or multiple modifiers to modify the value transferred.
-
-  Args:
-    - name: name of a link to recognize it on timeout.
-    - condition (optional): Children class of links.Condition.
-
-      Note:
-        Each "send" call will pass through the condition.evaluate method
-        and sends the returned value.
-
-        If condition has not evaluate method, send will try call condition.
-
-        You can pass a list of conditions, they will be executed in order.
-
-    - timeout (int or float, default: 0.1): Timeout for the send method.
-    - action ({'warn','kill','NoWarn',str}, default: "warn"): Action in case of
-      TimeoutError in the send method.
-
-      Note:
-        You can warn only, not warn or choose to kill the link. If any other
-        string, will be printed in case of error to debug.
-
+    You can add one or multiple :ref:`Modifiers` to modify the transferred
+    value.
   """
 
-  def __init__(self, input_block=None, output_block=None, condition=None,
-                modifier=None, timeout=0.1, action="warn", name="link"):
+  def __init__(self,
+               input_block=None,
+               output_block=None,
+               condition=None,
+               modifier=None,
+               timeout: float = 0.1,
+               action: str = "warn",
+               name: str = "link") -> None:
+    """Sets the instance attributes.
+
+    Args:
+      input_block:
+      output_block:
+      condition: Children class of :class:`links.Condition`, for backward
+        compatibility only. Can be a single condition or a :obj:`list` of
+        conditions that will be executed in the given order.
+      modifier:
+      timeout (:obj:`float`, optional): Timeout for the :meth:`send` method.
+      action (:obj:`str`, optional): Action to perform in case of a
+        :exc:`TimeoutError` during the :meth:`send` method. Should be in:
+        ::
+
+          'warn', 'kill', 'NoWarn',
+
+        any other value would be for debugging.
+      name (:obj:`str`, optional): Name of a link to recognize it on timeout.
+    """
 
     if modifier is None:
       modifier = []
@@ -122,14 +130,12 @@ class Link(object):
       input_block.add_output(self)
       output_block.add_input(self)
 
-  def close(self):
+  def close(self) -> None:
     self.in_.close()
     self.out_.close()
 
-  def send(self, value):
-    """
-    Send the value, or a modified value if you pass it through a
-    modifier.
+  def send(self, value: Any) -> None:
+    """Sends the value, or a modified value if you pass it through a modifier.
     """
 
     try:
@@ -150,7 +156,9 @@ class Link(object):
       raise
 
   @win_timeout(1)
-  def send_timeout(self, value):
+  def send_timeout(self, value: dict) -> None:
+    """Method for sending data with a given timeout on the link."""
+
     try:
       if self.modifiers is None or isinstance(value, str):
         self.out_.send(value)
@@ -172,22 +180,22 @@ class Link(object):
       raise
 
   @error_if_string  # Recv will raise an error if a string is received
-  def recv(self, blocking=True):
-    """
-    Receive data.
+  def recv(self, blocking: bool = True) -> Union[dict, None]:
+    """Receives data.
 
     Note:
-      If blocking=False, return None if there is no data
+      If ``blocking`` is :obj:`False`, returns :obj:`None` if there is no
+      data.
 
     Args:
-      - blocking: Enable (True) or disable (False) blocking mode.
+      blocking (:obj:`bool`, optional): Enables (:obj:`True`) or disables
+        (:obj:`False`) blocking mode.
 
     Returns:
-      If blocking is True, recv() method will wait
-      until data is available on the pipe and return the received data.
-      Otherwise, it will check if there is data available to return,
-      or return None if the pipe is empty.
-
+      If ``blocking`` is :obj:`True`, :meth:`recv` method will wait until data
+      is available in the pipe and return the received data. Otherwise, it will
+      check if there is data available to return, or return :obj:`None` if the
+      pipe is empty.
     """
 
     try:
@@ -211,25 +219,24 @@ class Link(object):
         self.in_.close()
       raise
 
-  def poll(self):
+  def poll(self) -> bool:
     return self.in_.poll()
 
-  def clear(self):
+  def clear(self) -> None:
     while self.in_.poll():
       self.in_.recv_bytes()
 
-  def recv_last(self, blocking=False):
-    """
-    Returns only the LAST value in the pipe, dropping all the others.
+  def recv_last(self, blocking: bool = False) -> Union[dict, None]:
+    """Returns only the LAST value in the pipe, dropping all the others.
 
     Note:
-      If blocking=False: Will return None if there is no data waiting.
+      If ``blocking`` is :obj:`False`, will return :obj:`None` if there is no
+      data waiting.
 
-      If blocking=True: Will wait for at least one data.
+      If ``blocking`` is :obj:`True`, will wait for at least one data.
 
-    Warning!
-      Unlike recv, default is NON blocking.
-
+    Warning:
+      Unlike :meth:`recv`, default is NON blocking.
     """
 
     if blocking:
@@ -240,16 +247,16 @@ class Link(object):
       data = self.recv()
     return data
 
-  def recv_chunk(self, length=0):
-    """
-    Allows you to receive a chunk of data:
-      If length > 0 it will return an OrderedDict containing LISTS of the last
-      length received data.
+  def recv_chunk(self, length: int = 0) -> dict:
+    """Allows you to receive a chunk of data.
 
-      If length=0, it will return all the waiting data until the pipe is empty.
+    If ``length`` `> 0` it will return a :obj:`dict` containing :obj:`list` of
+    the last length received data.
 
-      If the pipe is already empty, it will wait to return at least one value.
+    If ``length`` `= 0`, it will return all the waiting data until the pipe is
+    empty.
 
+    If the pipe is already empty, it will wait to return at least one value.
     """
 
     ret = self.recv()
@@ -270,19 +277,18 @@ class Link(object):
           raise IOError(str(self) + " Got data without label " + k)
     return ret
 
-  def recv_delay(self, delay):
-    """
-    Useful for blocks that don't need data all so frequently:
+  def recv_delay(self, delay: float) -> dict:
+    """Useful for blocks that don't need data all so frequently.
+
     It will continuously receive data for a given delay and return them as a
-    single OrderedDict containing lists of the values.
+    single :obj:`dict` containing :obj:`list` of the values.
 
     Note:
-      All the .recv calls are blocking so this method will take
-      AT LEAST delay seconds to return, but it could be more since it may wait
-      for data.
+      All the :meth:`recv` calls are blocking so this method will take AT LEAST
+      ``delay`` seconds to return, but it could be more since it may wait for
+      data.
 
       Also, it will return at least one reading.
-
     """
 
     t = time()
@@ -302,11 +308,9 @@ class Link(object):
           raise IOError(str(self) + " Got data without label " + k)
     return ret
 
-  def recv_chunk_nostop(self):
-    """
-    Experimental feature, to be used in finish methods to
-    recover the final remaining data (possibly after a stop signal).
-    """
+  def recv_chunk_nostop(self) -> Union[dict, None]:
+    """Experimental feature, to be used in :meth:`finish` methods to recover
+    the final remaining data (possibly after a stop signal)."""
 
     lst = []
     while self.in_.poll():
@@ -323,13 +327,11 @@ class Link(object):
     return r
 
 
-def link(in_block, out_block, **kwargs):
-  """
-  Function that links two blocks.
+def link(in_block, out_block, **kwargs) -> None:
+  """Function that links two blocks.
 
   Note:
     For the object, see :ref:`Link`.
-
   """
 
   Link(input_block=in_block, output_block=out_block, **kwargs)
