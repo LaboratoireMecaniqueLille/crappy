f26575243ee07fd4d63da70c91239fc98e472deb
André Auilo Martins <andre.auilo-martins@centrale.centralelille.fr>
2021-07-19 17:15:15 +0200

    [add] Added Motorkit pump actuator

--- crappy/actuator/__init__.py
+++ crappy/actuator/__init__.py
@@ -5,6 +5,7 @@ from .biaxe import Biaxe
 from .biotens import Biotens
 from .cmDrive import CM_drive
 from .fakemotor import Fake_motor
+from .motorkit_pump import Motorkit_pump
 from .oriental import Oriental
 from .servostar import Servostar
 from .pololu_tic import Pololu_tic
