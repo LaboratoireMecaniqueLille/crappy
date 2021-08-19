07bac6193dc3b4f78f01dc3ddf699b86ddb6e6c3
Antoine Weisrock <antoine.weisrock@gmail.com>
2021-08-19 16:23:54 +0200

    [ref] Updated the metadata in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import find_packages, __version__
+from setuptools import find_packages
 # To use a consistent encoding
 from codecs import open
 from os import popen, listdir, walk
@@ -103,13 +103,16 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version=__version__,
+  version='1.4.0',
 
   description='Command and Real-time Acquisition Parallelized in Python',
   long_description=long_description,
 
   # The project's main homepage.
   url='https://github.com/LaboratoireMecaniqueLille/crappy',
+
+  project_urls={'Documentation':
+                'https://crappy.readthedocs.io/en/latest/index.html'},
 
   # Author details
   author='LaMcube',
@@ -120,31 +123,14 @@ setup(
 
   zip_safe=False,
   # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-  classifiers=[
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 4 - Beta',
-
-    # Indicate who your project is intended for
-    'Intended Audience :: Science/Research',
-    'Topic :: Software Development :: Build Tools',
-
-    # Pick your license as you wish (should match "license" above)
-    'License :: OSI Approved :: '
-    'GNU General Public License v2 or later (GPLv2+)',
-
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    # 'Programming Language :: Python :: 2',
-    # 'Programming Language :: Python :: 2.6',
-    # 'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    # 'Programming Language :: Python :: 3.2',
-    # 'Programming Language :: Python :: 3.3',
-    'Operating System :: POSIX :: Linux',
-  ],
+  classifiers=['Development Status :: 5 - Production/Stable ',
+               'Intended Audience :: Science/Research',
+               'Topic :: Software Development :: Build Tools',
+               'License :: OSI Approved :: GNU General Public License v2 or '
+               'later (GPLv2+)',
+               'Programming Language :: Python :: 3.6',
+               'Natural Language :: English',
+               'Operating System :: OS Independent'],
 
   # What does your project relate to?
   keywords='control command acquisition multiprocessing',
@@ -152,6 +138,8 @@ setup(
   # You can just specify the packages manually here if your project is
   # simple. Or you can use find_packages().
   packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+  python_requires=">=3.6",
 
   ext_package='crappy',
   ext_modules=extensions,
