be75248efaebe497434a560f97527205629de846
Antoine Weisrock <antoine.weisrock@gmail.com>
2021-09-23 14:20:27 +0200

    [ref] Fixed warnings in the hello_class.cpp

--- sources/hello/hello_class.cpp
+++ sources/hello/hello_class.cpp
@@ -1,5 +1,6 @@
 #include <Python.h>
 #include <iostream>
+
 using namespace std;
 
 typedef struct {
@@ -7,16 +8,14 @@ typedef struct {
     char *name;
 } Hello;
 
-
-
-static PyObject *
-Hello_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+static PyObject* Hello_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
     Hello *self;
     self = (Hello *)type->tp_alloc(type, 0);
-    static char *kwlist[] = {"name", NULL};
+    static char *kwlist[] = {(char*)"name", NULL};
     if (self != NULL) {
-        if (! PyArg_ParseTupleAndKeywords(args, kwds, "|s", kwlist, &self->name)){
+        if (! PyArg_ParseTupleAndKeywords(args, kwds, "|s", kwlist,
+            &self->name)){
                 return NULL;
         }
     }
@@ -36,78 +35,41 @@ static int Hello_init(Hello *self, PyObject *args, PyObject *kwds)
 
 static void Hello_dealloc(Hello* self)
 {
-    self->ob_type->tp_free((PyObject*)self);
+    Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
 PyObject*
 Hello_get(Hello *self)
 {
-//    PyObject *ret_val =
-//    return Py_INCREF(ret_val);
     return Py_BuildValue("s", self->name);
 }
 
 PyObject*
 Hello_print(Hello *self)
 {
-
-	cout << "Hello " << self->name << endl;
-
-	Py_RETURN_NONE;
+    cout << "Hello " << self->name << endl;
+    Py_RETURN_NONE;
 }
 
 static PyMethodDef Hello_methods[] = {
-        {"say_hello", (PyCFunction)Hello_print, METH_VARARGS,
-	 "Say hello to somebody."},
-	 {"get_name", (PyCFunction)Hello_get, METH_NOARGS,
-	 "Return the name attribute."},
-    {NULL}
-};
-
-static PyMethodDef module_methods[] = {
+    {"say_hello", (PyCFunction)Hello_print, METH_VARARGS,
+     "Say hello to somebody."},
+    {"get_name", (PyCFunction)Hello_get, METH_NOARGS,
+    "Return the name attribute."},
     {NULL}
 };
 
 static PyTypeObject helloType = {
     PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "Hello",             /*tp_name*/
-    sizeof(Hello),             /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)Hello_dealloc, /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "Hello objects",           /* tp_doc */
-    0,		               /* tp_traverse */
-    0,		               /* tp_clear */
-    0,		               /* tp_richcompare */
-    0,		               /* tp_weaklistoffset */
-    0,		               /* tp_iter */
-    0,		               /* tp_iternext */
-    Hello_methods,             /* tp_methods */
-    0,             /* tp_members */
-    0,                         /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    (initproc)Hello_init,      /* tp_init */
-    0,                         /* tp_alloc */
-    Hello_new,                 /* tp_new */
+    .tp_name = "crappy.tool.Hello",
+    .tp_basicsize = sizeof(Hello),
+    .tp_itemsize = 0,
+    .tp_dealloc = (destructor) Hello_dealloc,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_doc = "Hello objects",
+    .tp_methods = Hello_methods,
+    .tp_init = (initproc) Hello_init,
+    .tp_new = Hello_new,
 };
 
 static struct PyModuleDef helloClassModule = {
