480528e6851305cba7035019496e75f58b7fdd9e
Antoine Weisrock <antoine.weisrock@gmail.com>
2022-04-22 14:06:06 +0200

    [ref] Complete refactoring of the Multiplex block, making it clearer and a bit more efficient

--- crappy/blocks/multiplex.py
+++ crappy/blocks/multiplex.py
@@ -1,127 +1,168 @@
 # coding: utf-8
 
-import numpy as np
-from typing import Union
+from numpy import interp, array, searchsorted, concatenate
+from typing import NoReturn
 from itertools import chain
-# Todo: interp nearest
 
 from .block import Block
 
 
-def interp(xp: list, yp: Union[range, list], x: float) -> float:
-  try:
-    return np.interp(x, xp, yp)
-  except (TypeError, ValueError):
-    # Make a nearest interpolation for non numerical values
-    a = range(len(yp))
-    return yp[int(np.interp(x, xp, a) + .5)]
-
-
 class Multiplex(Block):
-  """This block interpolates data.
+  """This block takes data from upstream blocks as input and interpolates it to
+  output all labels in a common time basis.
 
-  It is used to read data and return the reading from multiple sensors at the
-  same instants and a constant frequency. This block uses linear interpolation
-  whenever possible, else the nearest neighbor.
+  It is useful for synchronizing data acquired from different sensors, e.g. to
+  plot a real-time stress-strain curve. This block is however quite
+  resource-consuming, so it is preferable to perform interpolation in
+  post-processing if real-time is not needed.
 
-  Warning:
-    This block  needs a delay to make sure all the required data for
-    interpolation has already been received, so do not use this block as the
-    input of a decision block!
+  Note:
+    This block doesn't truly output data in real-time as it needs to wait for
+    data from all the upstream blocks before performing the interpolation.
+    So it should only be used with care when it is an input of a
+    decision-making block. This is especially true when the upstream blocks
+    have very different sample rates.
   """
 
-  def __init__(self, key: str = 't(s)', freq: float = 200) -> None:
+  def __init__(self,
+               time_label: str = 't(s)',
+               freq: float = 200,
+               verbose: bool = False) -> None:
     """Sets the args and initializes the parent class.
 
     Args:
-      key (:obj:`str`, optional): The label carrying the time information.
-      freq (:obj:`float`, optional): The block will loop at this frequency.
+      time_label: The label carrying the time information.
+      freq : The sample rate for the interpolation, and the target looping
+        frequency for the block. If this value is set too high and your machine
+        cannot keep up, the block will most likely lag.
+      verbose: If :obj:`True`, prints information about the looping frequency
+        of the block.
     """
 
     Block.__init__(self)
-    self.k = key
+
+    # Initializing the attributes
+    self._time_label = time_label
     self.freq = freq
-    self.hist = dict()
-    # Dict, keys = labels, values = list of the values
-    self.t_hist = []
-    # ith element is a list containing a list of the timestamps from
-    # the ith link
-    self.label_list = []
-    # ith element is a list containing a list of the labels to get from
-    # the ith link
-    self.t = 0
-    self.dt = 1 / self.freq
+    self.verbose = verbose
+    self._t = 0
+    self._dt = 1 / self.freq
 
-  def begin(self) -> None:
-    """We need to receive the first bit of data from each input to know the
-    labels and make lists of what we will read in the main loop."""
+    # Creating the different dicts holding information
+    self._values = dict()
+    self._timestamps = dict()
+    self._labels_to_get = dict()
+    self._label_to_link = dict()
 
-    for i, link in enumerate(self.inputs):
-      r = link.recv_chunk()
-      if self.k not in r:  # This input don't have the timebase label!
-        self.t_hist.append([])
-        self.label_list.append([])
+  def begin(self) -> NoReturn:
+    """Receiving the first data from the upstream blocks, and checking that it
+    is valid.
+
+    If part of the data is not valid, warning the user. For the valid data,
+    initializing the different dicts with it.
+    """
+
+    for link in self.inputs:
+      # First, receiving data from each incoming link
+      data = link.recv_chunk()
+
+      # Handling the case when the time label is absent from the data
+      if self._time_label not in data:
+        print(f'WARNING : Cannot multiplex data coming from link {link.name} '
+              f'as it does not contain the time label ({self._time_label})')
         continue
-      self.t_hist.append(r[self.k])  # Append the time to this link's history
-      labels = []  # To recap all the labels we will use at each loop
-      for k in r:
-        if k == self.k:  # Ignore the timebase label (treated separately)
-          continue
-        if k in chain(*self.label_list):
-          print("WARNING, label %s comes from multiple inputs, "
-                "ignoring one" % k)
-          continue
-        labels.append(k)
-        self.hist[k] = r[k]  # Put the data in the hist dict
-      self.label_list.append(labels)  # Add the label list of this link
 
-  def get_data(self) -> None:
-    for i, l in enumerate(self.label_list):
-      r = self.inputs[i].recv_chunk()  # Get the data
-      if not l:
-        continue
-      self.t_hist[i].extend(r[self.k])  # Add the time to this link's history
-      for k in l:
-        self.hist[k].extend(r[k])  # Add each data to their history
+      # Extracting the timestamps from the received data
+      timestamp = data.pop(self._time_label)
 
-  def send_data(self) -> None:
-    while all([i and i[-1] > self.t for i in self.t_hist]) \
-      and self.t_hist:  # Send all we can
-      r = {self.k: self.t}  # First data to return: our new timebase
-      for i, l in enumerate(self.label_list):
-        if not l:
-          continue
-        for k in l:
-          r[k] = interp(self.t_hist[i], self.hist[k], self.t)  # Interpolate
-        # To know until when we can delete
-        last = int(interp(self.t_hist[i], range(len(self.t_hist[i])),
-                   self.t + self.dt))
-        # Delete the data we don't need anymore
-        self.t_hist[i] = self.t_hist[i][last:]
-        for k in l:
-          self.hist[k] = self.hist[k][last:]
-      self.send(r)  # Send it!
-      self.t += self.dt  # And increment our timebase
+      # Determining which labels to use for multiplexing and warning the user
+      # if two similar labels found
+      self._labels_to_get[link] = [label for label in data if label not in
+                                   chain(*self._labels_to_get.values())]
+      if len(self._labels_to_get[link]) != len(data):
+        print(f"WARNING : Got identical label(s)" 
+              f"""{tuple(label for label in data if label 
+                         not in self._labels_to_get[link])}""" 
+              f"from at least two different links, on of them won't be used "
+              f"for multiplexing.")
 
-  def loop(self) -> None:
-    self.get_data()
-    self.send_data()
+      self._label_to_link.update({label: link for label in
+                                  self._labels_to_get[link]})
 
-  def finish(self) -> None:
-    self.send_data()
-    while any([len(k) > 1 for k in self.t_hist]):
-      r = {self.k: self.t}  # First data to return: our new timebase
-      for i, l in enumerate(self.label_list):
-        if not l:
-          continue
-        for k in l:
-          r[k] = interp(self.t_hist[i], self.hist[k], self.t)  # Interpolate
-        # To know until when we can delete
-        last = int(interp(self.t_hist[i], range(len(self.t_hist[i])),
-                   self.t + self.dt))
-        # Delete the data we don't need anymore
-        self.t_hist[i] = self.t_hist[i][last:]
-        for k in l:
-          self.hist[k] = self.hist[k][last:]
-      self.send(r)  # Send it!
-      self.t += self.dt  # And increment our timebase
+      # Storing the timestamps for the link
+      self._timestamps[link] = array(timestamp)
+      # Storing the values for the labels that were kept
+      self._values.update({label: array(values) for label, values in
+                           data.items() if label in self._labels_to_get[link]})
+
+  def loop(self) -> NoReturn:
+    """Receives data, interpolates it, and sends it to the downstream
+    blocks."""
+
+    self._get_data()
+    self._send_data()
+
+  def finish(self) -> NoReturn:
+    """Just sending any remaining data."""
+
+    self._send_data()
+
+  def _get_data(self) -> NoReturn:
+    """Receives data from the upstream links."""
+
+    for link in self.inputs:
+      # Receiving data from each link, non-blocking to prevent accumulation
+      data = link.recv_chunk(blocking=False)
+      # Processing only the valid labels
+      if data is not None and link in self._labels_to_get:
+        # Saving the timestamps
+        self._timestamps[link] = concatenate((self._timestamps[link],
+                                              array(data[self._time_label])))
+        # Saving the other values
+        for label in self._labels_to_get[link]:
+          self._values[label] = concatenate((self._values[label],
+                                             array(data[label])))
+
+  def _send_data(self) -> NoReturn:
+    """Interpolates the previously received data, and sends the result to the
+    downstream blocks."""
+
+    # Making sure all the necessary data has been received for interpolating
+    if all(timestamp[-1] > self._t for timestamp
+           in self._timestamps.values()) and any(
+      timestamp.size for timestamp in self._timestamps.values()):
+
+      # Getting the maximum timestamp common to all the labels
+      max_t = min(times[-1] for times in self._timestamps.values())
+      # Deducing the number of time intervals to interpolate on
+      n_samples = int((max_t - self._t) // self._dt) + 1
+      # Creating the array of timestamps for interpolation
+      new_times = [self._t + i * self._dt for i in range(n_samples)]
+
+      # For each link, getting the index for trimming data after interpolation
+      last_indexes = {link: searchsorted(self._timestamps[link], new_times[-1],
+                                         side='right')
+                      for link in self.inputs}
+
+      # This dict stores the values to send
+      to_send = {self._time_label: new_times}
+
+      # For each label, interpolating the data
+      for label in self._values:
+        link = self._label_to_link[label]
+        to_send[label] = list(interp(new_times, self._timestamps[link],
+                                     self._values[label]))
+
+        # Trimming the values to save some memory
+        self._values[label] = self._values[label][last_indexes[link]:]
+
+      # Also trimming the timestamps to save some memory
+      for link in self.inputs:
+        self._timestamps[link] = self._timestamps[link][last_indexes[link]:]
+
+      # Updating the current time value
+      self._t = new_times[-1] + self._dt
+
+      # Finally, sending the data to downstream blocks
+      while any(to_send.values()):
+        self.send({label: values.pop(0) for label, values in to_send.items()})
