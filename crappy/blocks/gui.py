d51b03870431c44fa25f6605dfca3ef071b1014c
Antoine Weisrock <antoine.weisrock@gmail.com>
2022-05-12 09:55:40 +0200

    [ref] Refactored the GUI block
    It now has a nicer layout, is less sensitive to crashes, more easily readable, and a few features were added.

--- crappy/blocks/gui.py
+++ crappy/blocks/gui.py
@@ -3,7 +3,6 @@
 from time import time
 
 from .block import Block
-from .._global import CrappyStop
 from .._global import OptionalModule
 
 try:
@@ -13,46 +12,82 @@ except (ModuleNotFoundError, ImportError):
 
 
 class GUI(Block):
-  """Block to send a signal based on a user input."""
+  """This block allows the user to send a signal upon clicking on a button in
+  a graphical user interface.
+
+  It sends an integer value, that starts from `0` and is incremented every time
+  the user clicks on the button.
+  """
 
   def __init__(self,
-               freq: float = 50,
+               send_0: bool = False,
                label: str = 'step',
-               spam: bool = False) -> None:
+               time_label: str = 't(s)',
+               freq: float = 50,
+               spam: bool = False,) -> None:
+    """Sets the args and initializes the parent class."""
+
     Block.__init__(self)
     self.freq = freq
-    self.spam = spam  # Send the values only once or at each loop ?
-    self.i = 0  # The value to be sent
-    self.abort = False
-    if isinstance(label, list):
-      self.labels = label
-    else:
-      self.labels = ['t(s)', label]
+    self.spam = spam
+    self.labels = [time_label, label]
+
+    self._send_0 = send_0
 
   def prepare(self) -> None:
-    self.root = tk.Tk()
-    self.root.title("GUI block")
-    self.root.protocol("WM_DELETE_WINDOW", self.end)
-    self.label = tk.Label(self.root, text='step: 0')
-    self.label.pack()
-    self.button = tk.Button(self.root, text='Next step', command=self.callback)
-    self.button.pack()
-    self.send([0, self.i])
+    """Creates the graphical interface and sets its layout and callbacks."""
+
+    self._root = tk.Tk()
+    self._root.title("GUI block")
+    self._root.resizable(False, False)
+
+    self._step = tk.IntVar()
+    self._step.trace_add('write', self._update_text)
+    self._text = tk.StringVar(value=f'step: {self._step.get()}')
+
+    self._label = tk.Label(self._root, textvariable=self._text)
+    self._label.pack(padx=7, pady=7)
+
+    self._button = tk.Button(self._root,
+                             text='Next step',
+                             command=self._next_step)
+    self._button.pack(padx=25, pady=7)
+
+    self._root.update()
+
+  def begin(self) -> None:
+    """Sends the value of the first step (`0`) if required."""
+
+    if self._send_0:
+      self.send([time() - self.t0, self._step.get()])
 
   def loop(self) -> None:
+    """Updates the interface, and sends the current step value if spam is
+    :obj:`True`. """
+
+    try:
+      self._root.update()
+    except tk.TclError:
+      return
+
     if self.spam:
-      self.send([time() - self.t0, self.i])
-    if self.abort:
-      raise CrappyStop
-    self.root.update()
-
-  def end(self) -> None:
-    self.abort = True
-
-  def callback(self) -> None:
-    self.i += 1
-    self.send([time()-self.t0, self.i])
-    self.label.configure(text='step: %d' % self.i)
+      self.send([time() - self.t0, self._step.get()])
 
   def finish(self) -> None:
-    self.root.destroy()
+    """Closes the interface window."""
+
+    try:
+      self._root.destroy()
+    except tk.TclError:
+      pass
+
+  def _update_text(self, _, __, ___) -> None:
+    """Simply updates the displayed text."""
+
+    self._text.set(f'step: {self._step.get()}')
+
+  def _next_step(self) -> None:
+    """Increments the step counter and sends the corresponding signal."""
+
+    self._step.set(self._step.get() + 1)
+    self.send([time() - self.t0, self._step.get()])
