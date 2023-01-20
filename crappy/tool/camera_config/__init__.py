bc07e16b3bb77bc7efac5dc94364720e26b9e4de
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-01-20 15:36:30 +0100

    [ref] Renamed files in the tool/camera_config folder

--- crappy/tool/camera_config/__init__.py
+++ crappy/tool/camera_config/__init__.py
@@ -2,7 +2,7 @@
 
 from .camera_config import CameraConfig
 from .camera_config_boxes import CameraConfigBoxes
-from .discorrel_config import DISCorrelConfig
-from .disve_config import DISVEConfig
+from .dis_correl_config import DISCorrelConfig
+from .dis_ve_config import DISVEConfig
 from .video_extenso_config import VideoExtensoConfig
 from .config_tools import Box, SpotsBoxes, SpotsDetector, Zoom
