ce5b6631795582bbe7fc8ae25aeb90a1c1db03a9
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-04-14 16:55:47 +0200

    [ref] Reorganized the imports in alphabetical order

--- src/crappy/actuator/__init__.py
+++ src/crappy/actuator/__init__.py
@@ -2,16 +2,17 @@
 
 from typing import Dict, Type
 
+from .adafruit_dc_motor_hat import DCMotorHat
+from .fake_motor import FakeMotor
+from .jvl_mac_140 import JVLMac140
+from .kollmorgen_servostar_300 import ServoStar300
+from .newport_tra6ppd import NewportTRA6PPD
+from .oriental_ard_k import OrientalARDK
+from .pololu_tic import PololuTic
+from .schneider_mdrive_23 import SchneiderMDrive23
+
 from .ft232h import DCMotorHatFT232H
 
 from .meta_actuator import MetaActuator, Actuator
-from .jvl_mac_140 import JVLMac140
-from .schneider_mdrive_23 import SchneiderMDrive23
-from .fake_motor import FakeMotor
-from .adafruit_dc_motor_hat import DCMotorHat
-from .oriental_ard_k import OrientalARDK
-from .kollmorgen_servostar_300 import ServoStar300
-from .pololu_tic import PololuTic
-from .newport_tra6ppd import NewportTRA6PPD
 
 actuator_dict: Dict[str, Type[Actuator]] = MetaActuator.classes
