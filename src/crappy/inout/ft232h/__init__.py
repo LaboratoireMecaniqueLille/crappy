8b450fcdbb8a6cd63af2c15b07325d9621e5f2aa
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-05-01 17:01:56 +0200

    [ref] Removed the ft232h mention in the InOut file names

--- src/crappy/inout/ft232h/__init__.py
+++ src/crappy/inout/ft232h/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-from .ads1115_ft232h import ADS1115FT232H
-from .gpio_switch_ft232h import GPIOSwitchFT232H
-from .mcp9600_ft232h import MCP9600FT232H
-from .mprls_ft232h import MPRLSFT232H
-from .nau7802_ft232h import NAU7802FT232H
-from .waveshare_ad_da_ft232h import WaveshareADDAFT232H
+from .ads1115 import ADS1115FT232H
+from .gpio_switch import GPIOSwitchFT232H
+from .mcp9600 import MCP9600FT232H
+from .mprls import MPRLSFT232H
+from .nau7802 import NAU7802FT232H
+from .waveshare import WaveshareADDAFT232H
