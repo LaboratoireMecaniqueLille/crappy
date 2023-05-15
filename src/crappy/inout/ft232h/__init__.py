94e045fcd305fee97b4596b964b2683ac48d21ea
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-05-15 22:51:07 +0200

    [ref] Renamed file that wasn't named correctly

--- src/crappy/inout/ft232h/__init__.py
+++ src/crappy/inout/ft232h/__init__.py
@@ -5,4 +5,4 @@ from .gpio_switch import GPIOSwitchFT232H
 from .mcp9600 import MCP9600FT232H
 from .mprls import MPRLSFT232H
 from .nau7802 import NAU7802FT232H
-from .waveshare import WaveshareADDAFT232H
+from .waveshare_ad_da import WaveshareADDAFT232H
