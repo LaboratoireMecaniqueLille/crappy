4a61668d2a24089e197c7d8748668d55183373d2
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-06-11 17:03:32 +0200

    [ref] Integrated the ApplyStrainToImage tool to the examples
    This tool was newly added in commit 4aa81068

--- examples/blocks/dic_ve/dic_ve_no_config.py
+++ examples/blocks/dic_ve/dic_ve_no_config.py
@@ -11,49 +11,6 @@ for more information.
 """
 
 import crappy
-import numpy as np
-import cv2
-
-
-class ApplyStrainToImage:
-  """This class reshapes an image depending on input strain values. It is meant
-  to simulate the stretching of a sample during a tensile test.
-
-  You don't have to care too much about what it contains, it is not needed for
-  understanding the example.
-  """
-
-  def __init__(self,
-               image: np.ndarray) -> None:
-    """Sets the base image and initializes the necessary objects to use."""
-
-    self._img = image
-
-    # Building the lookup arrays for the cv2.remap method
-    height, width, *_ = image.shape
-    orig_x, orig_y = np.meshgrid(range(width), range(height))
-    # These arrays correspond to the original state of the image
-    self._orig_x = orig_x.astype(np.float32)
-    self._orig_y = orig_y.astype(np.float32)
-
-    # These arrays are meant to be added to the original image ones
-    # If added as is, they correspond to a 100% strain state in both directions
-    self._x_strain = self._orig_x * width / (width - 1) - width / 2
-    self._y_strain = self._orig_y * height / (height - 1) - height / 2
-
-  def __call__(self, exx: float, eyy: float) -> np.ndarray:
-    """Returns the reshaped image, based on the given strain values."""
-
-    exx /= 100
-    eyy /= 100
-
-    # The final lookup table is the sum of the original state ones plus the
-    # 100% strain one weighted by a ratio
-    transform_x = self._orig_x - (exx / (1 + exx)) * self._x_strain
-    transform_y = self._orig_y - (eyy / (1 + eyy)) * self._y_strain
-
-    return cv2.remap(self._img, transform_x, transform_y, 1)
-
 
 if __name__ == '__main__':
 
@@ -93,8 +50,9 @@ if __name__ == '__main__':
       # patches on the speckle image
       freq=50,  # Lowering the default frequency because it's just a demo
       save_images=False,  # We don't want images to be recorded in this demo
-      image_generator=ApplyStrainToImage(img),  # This argument makes the Block
-      # generate fake strain on the given image, only useful for demos
+      image_generator=crappy.tool.ApplyStrainToImage(img),  # This argument
+      # makes the Block generate fake strain on the given image, only useful
+      # for demos
       patches=[(100, 224, 64, 64), (224, 348, 64, 64),
                (348, 224, 64, 64), (224, 100, 64, 64)],  # Providing here the
       # patches to track as they cannot be selected in the configuration window
