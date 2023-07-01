7eb18b29906ace89f10163fbe624bcc2c7782653
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-07-01 19:51:17 +0200

    [add] Added well-commented examples to the examples folder
    Added examples for the Demux, Diff and Integrate Modifiers

---
+++ examples/modifiers/demux.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+"""
+The ioblock/ioblock_streamer.py example already demonstrates the use of the
+Demux Modifier in the most basic use case. As it would not be relevant to
+copy-paste the same code here, please refer to the mentioned example instead.
+"""
