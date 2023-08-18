ea40cb13fa5bdd6479e6e4b792d3a6c685893dc9
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-08-18 19:06:46 +0200

    [ref] Refactored to match the syntax of all the other examples

--- examples/other_examples/labjack_t7.py
+++ examples/other_examples/labjack_t7.py
@@ -1,69 +1,61 @@
 # coding: utf-8
 
 """
-Detailed example showing a simple tensile test.
-
-This example uses a Labjack T7 board to send the position command to a tensile
-machine. This was tested on an Instron 5882, but any tensile machine taking an
-external command can be used.
-
-Required hardware:
-  - Tensile machine able to take an external command
-  - DAQ board (here a Labjack T7)
+This example demonstrates the use of a Labjack in Crappy for acquiring data
+from digital inputs and driving the digital outputs. It is presented here
+because we want to promote the use of Labjack equipment, that we use and
+appreciate in our laboratory. It is recommended to first read and use the
+blocks/ioblock/*.py examples before starting this one. This example requires
+the Python module labjack and matplotlib to be installed. It also necessitates
+a working Labjack T7.
 """
 
 import crappy
 
 if __name__ == "__main__":
-  SPEED = 5  # mm/min
-  FORCE_GAIN = 100  # N/V
-  POS_GAIN = 5  # mm/V
 
-  # The path we will use in the generator
-  path = {
-    "type": "Ramp",
-    "speed": SPEED / 60,  # Convert to mm/s (in ramps, speed is always
-    # in unit/s)
-    "condition": None}  # No exit condition: we will stop the test manually
+  # This Generator Block generates a ramp signal that drives the digital output
+  # of the Labjack
+  gen = crappy.blocks.Generator(
+      # Generating a linearly increasing signal
+      ({"type": "Ramp", "speed": 5 / 60, "condition": None},),
+      cmd_label='cmd',  # The label carrying the generated signal
+      freq=30,  # Lowering the default frequency because it's just a demo
 
-  # Let's create the first block: the generator
-  gen = crappy.blocks.Generator([path], cmd_label='cmd')
+      # Sticking to default for the other arguments
+  )
 
-  # Now the DAQ board we will interact with. Let's define the channels first
-  # First input: the force sensor
-  force = {'name': 'AIN0', 'gain': FORCE_GAIN}
-  # Second input: the position sensor
-  # make_zero asks the board to read the value at the beginning and subtract
-  # if from the measured value to make it relative to the beginning of the test
-  pos = {'name': 'AIN1', 'gain': POS_GAIN, 'make_zero': True}
-  # Only output: the command sent to the machine
-  # Because TDACx is necessarily an output and AINx an input, the block will
-  # guess the direction of each channel. If ambiguous it must be specified with
-  # direction=...  (False for input, True for output)
-  cmd = {'name': 'TDAC0', 'gain': POS_GAIN}
-  # Now let us create the block: the first arg is the name of the InOut object
-  # to use. The block will instantiate it itself ! No need to create it here
-  # Then we give the channels, the InOut will automatically detect that it has
-  # two inputs and one output
-  # The output labels always start with the time in second so we need 3 outputs
-  daq = crappy.blocks.IOBlock('LabjackT7',
-                              channels=[force, pos, cmd],
-                              labels=['t(s)', 'F(N)', 'Position(mm)'],
-                              cmd_labels=['cmd'])
-  # Now we link them: the output of the board will be read from the label 'cmd'
-  # Note that links are one-way only, so the order is important
+  # This IOBlock drives a Labjack acquisition board. It acquires data from the
+  # AIN0 and AIN1 ports, and sends it to the Grapher Block for display. It also
+  # drives the TDAC0 output based on the command received from the Generator
+  # Block
+  daq = crappy.blocks.IOBlock(
+      'LabjackT7',  # The name of the InOut to drive
+      # This dictionary contains the information needed to set up the channels
+      # to use on the Labjack
+      channels=[{'name': 'AIN0', 'gain': 1},
+                {'name': 'AIN1', 'gain': 1, 'make_zero': True},
+                {'name': 'TDAC0', 'gain': 1}],
+      labels=['t(s)', 'AIN0', 'AIN1'],  # The labels to send to downstream
+      # Blocks carrying the acquired signals
+      cmd_labels=['cmd'],  # The label carrying the command to set on TDAC0
+      freq=50,  # Lowering the default frequency because it's just a demo
+
+      # Sticking to default for the other arguments
+  )
+
+  # This Grapher Block displays the signals acquired by the Labjack on the AIN0
+  # and AIN1 channels
+  graph = crappy.blocks.Grapher(
+      # The names of the labels to plot on the graph
+      ('t(s)', 'AIN0'), ('t(s)', 'AIN1'),
+
+      # Sticking to default for the other arguments
+  )
+
+  # Linking the Block so that the information is correctly sent and received
   crappy.link(gen, daq)
-
-  # Now we would like to save the measurements from the machine
-  rec = crappy.blocks.Recorder('results.csv')
-  crappy.link(daq, rec)
-
-  # And why not display the force in real time during the test
-  graph = crappy.blocks.Grapher(('t(s)', 'F(N)'))
   crappy.link(daq, graph)
 
-  # Now that all the blocks are created and linked, the test can be started
-  # Do not forget to enable the external command on the machine beforehand
+  # Mandatory line for starting the test, this call is blocking
   crappy.start()
-  # To interrupt the test, simply press ctrl+C in the terminal
-  # It will stop all the blocks and end the program
