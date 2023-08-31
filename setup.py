f95d8c79408dd50babba159f77d3d6bcf90ef6b0
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-08-31 22:13:31 +0200

    [ref] Removed support for Python 3.11 in setup.py

--- setup.py
+++ setup.py
@@ -114,7 +114,6 @@ setup(
                'Programming Language :: Python :: 3.8',
                'Programming Language :: Python :: 3.9',
                'Programming Language :: Python :: 3.10',
-               'Programming Language :: Python :: 3.11',
                'Topic :: Scientific/Engineering',
                'Topic :: Software Development :: Build Tools',
                'Topic :: Software Development :: Embedded Systems'],
