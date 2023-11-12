9ca14c2c186a8bd3ed82ef242daed07583b2a007
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-11-12 16:37:53 +0100

    docs: add 3.11 and 3.12 Python version to list of supported ones on PyPI

--- setup.py
+++ setup.py
@@ -104,6 +104,8 @@ setup(
                'Programming Language :: Python :: 3.8',
                'Programming Language :: Python :: 3.9',
                'Programming Language :: Python :: 3.10',
+               'Programming Language :: Python :: 3.11',
+               'Programming Language :: Python :: 3.12',
                'Topic :: Scientific/Engineering',
                'Topic :: Software Development :: Build Tools',
                'Topic :: Software Development :: Embedded Systems'],
