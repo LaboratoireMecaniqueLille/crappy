ecbdfc284b44d14316ddecf3212cde3a3c1ca0ce
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-12-16 23:09:45 +0100

    ref: add a few more test cases

--- tests/camera_configuration/test_01_exit.py
+++ tests/camera_configuration/test_01_exit.py
@@ -11,13 +11,23 @@ class TestFinish(ConfigurationWindowTestBase):
   def test_exit(self) -> None:
     """"""
 
+    # The stop event should not be set
+    self.assertFalse(self._config._stop_event.is_set())
+
+    # The histogram process should still be alive
+    self.assertTrue(self._config._histogram_process.is_alive())
+
     # Destroying the main window
     self._config.finish()
+
+    # The stop event should be set
+    self.assertTrue(self._config._stop_event.is_set())
 
     # This call should raise an error as the window shouldn't exist anymore
     with self.assertRaises(TclError):
       self._config.wm_state()
 
+    # The histogram process should have been killed
     self.assertFalse(self._config._histogram_process.is_alive())
 
     # Indicating the tearDown() method not to destroy the window
