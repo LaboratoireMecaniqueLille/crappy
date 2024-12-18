fa057af66280f794e17f664a4a4f987cc8e47185
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-12-18 22:39:12 +0100

    tests: add test for auto range feature in camera config window

---
+++ tests/camera_configuration/test_11_auto_range.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+from .camera_configuration_test_base import (ConfigurationWindowTestBase,
+                                             FakeTestCameraSimple)
+
+class TestAutoRange(ConfigurationWindowTestBase):
+  """"""
+
+  def __init__(self, *args, **kwargs) -> None:
+    """"""
+
+    super().__init__(*args,
+                     camera=FakeTestCameraSimple(min_val=3, max_val=252),
+                     **kwargs)
+
+  def test_auto_range(self) -> None:
+    """"""
+
+    # The auto range feature should be disabled by default
+    self.assertFalse(self._config._auto_range.get())
+
+    # Calling the first loop
+    self._config._img_acq_sched()
+    self._config._upd_var_sched()
+    self._config._upd_sched()
+
+    # Both stored images should have bounds at 3 and 255
+    self.assertEqual(int(self._config._img.min()), 3)
+    self.assertEqual(int(self._config._img.max()), 252)
+    self.assertEqual(int(self._config._original_img.min()), 3)
+    self.assertEqual(int(self._config._original_img.max()), 252)
+
+    # Enable the auto range feature
+    self._config._auto_range_button.invoke()
+
+    # The auto range feature should be enabled
+    self.assertTrue(self._config._auto_range.get())
+
+    # Call a second loop
+    self._config._img_acq_sched()
+    self._config._upd_var_sched()
+    self._config._upd_sched()
+
+    # The original image should have the original limits, but the other one
+    # should have been extended to use the full range
+    self.assertEqual(int(self._config._img.min()), 0)
+    self.assertEqual(int(self._config._img.max()), 255)
+    self.assertEqual(int(self._config._original_img.min()), 3)
+    self.assertEqual(int(self._config._original_img.max()), 252)
+
+    # Disable the auto range feature
+    self._config._auto_range_button.invoke()
+
+    # The auto range feature should be disabled
+    self.assertFalse(self._config._auto_range.get())
+
+    # Call a third loop
+    self._config._img_acq_sched()
+    self._config._upd_var_sched()
+    self._config._upd_sched()
+
+    # The behavior should be back to default
+    self.assertEqual(int(self._config._img.min()), 3)
+    self.assertEqual(int(self._config._img.max()), 252)
+    self.assertEqual(int(self._config._original_img.min()), 3)
+    self.assertEqual(int(self._config._original_img.max()), 252)
