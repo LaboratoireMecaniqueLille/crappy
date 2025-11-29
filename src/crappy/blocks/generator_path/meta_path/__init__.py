e00a2af6ebb80a1abe0ac63ceb8c643035a31165
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-11-29 16:35:22 +0100

    ref: forgot to update type hints in commit of previous merge

--- src/crappy/blocks/generator_path/meta_path/__init__.py
+++ src/crappy/blocks/generator_path/meta_path/__init__.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-from typing import Dict, Type
-
 from .path import Path, ConditionType
 from .meta_path import MetaPath
 
-paths_dict: Dict[str, Type[Path]] = MetaPath.classes
+paths_dict: dict[str, type[Path]] = MetaPath.classes
