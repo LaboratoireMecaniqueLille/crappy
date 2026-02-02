1daa389ab1be57aa47844271e3909188a25bc9c9
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-02-02 16:01:19 +0100

    ref: reference test classes in __init__.py

--- tests/block/__init__.py
+++ tests/block/__init__.py
@@ -1,3 +1,6 @@
 # coding: utf-8
 
-
+from .test_01_class_api import TestClassAPI
+from .test_02_startup_sequence import TestStartupSequence
+from .test_03_run_cycle import TestRunCycle
+from .test_04_pause_freq import TestPauseFreq
