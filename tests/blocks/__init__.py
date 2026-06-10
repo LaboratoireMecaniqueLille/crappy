3ca6e9799da7b201a70cd0a46c4ffcb26f8799af
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-06-11 00:02:16 +0200

    tests: add __init__.py file

---
+++ tests/blocks/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from .test_fake_machine import TestFakeMachine
+from .test_link_reader import TestLinkReader
+from .test_mean import TestMeanBlock
+from .test_multiplexer import TestMultiplexer
+from .test_pid import TestPID
+from .test_recorder import TestRecorder
+from .test_sink import TestSink
+from .test_synchronizer import TestSynchronizer
+from .test_ucontroller import TestUController
