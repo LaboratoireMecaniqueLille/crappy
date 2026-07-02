da6943f0b2485951021b418695827be3533c6810
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-07-02 19:40:47 +0200

    fix: incorrect data type in case a dict is returned

--- src/crappy/inout/meta_inout/inout.py
+++ src/crappy/inout/meta_inout/inout.py
@@ -413,7 +413,7 @@ class InOut:
         # Case when get_data returns a dict
         elif (self._compensations_dict and
               len(self._compensations_dict) == array.shape[1]):
-          comp.append(array + self._compensations_dict.values())
+          comp.append(array + list(self._compensations_dict.values()))
         # The shapes do not match
         else:
           raise ValueError("The number of offsets doesn't match the shape "
