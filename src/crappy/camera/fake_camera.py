31feb80708be0b81ffa2f5ec6bbdd4232bdef61a
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-07-02 19:37:43 +0200

    ref: make default in setting match kwarg default in open()

--- src/crappy/camera/fake_camera.py
+++ src/crappy/camera/fake_camera.py
@@ -27,7 +27,7 @@ class FakeCamera(Camera):
 
     self.add_scale_setting('width', 1, 4096, None, self._gen_image, 1280, 1)
     self.add_scale_setting('height', 1, 4096, None, self._gen_image, 720, 1)
-    self.add_scale_setting('speed', 0., 800., None, None, 400., 0.8)
+    self.add_scale_setting('speed', 0., 800., None, None, 100., 0.8)
     self.add_scale_setting('fps', 0.1, 100., None, None, 50., 0.1)
 
     self._t0 = time()
