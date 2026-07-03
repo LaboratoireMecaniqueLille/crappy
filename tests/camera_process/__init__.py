2fe5997e77e13a42742eb7a9b0618e430a0d7524
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-07-03 15:32:31 +0200

    tests: move tests from camera_processes to camera_process

---
+++ tests/camera_process/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+from .test_01_class_api import TestClassAPI
+from .test_02_shared import TestSharedObjects
+from .test_03_run_cycle import TestRunCycle
+from .test_04_data import TestData
+from .test_05_links import TestLinks
+from .test_06_logging import TestLogging
+from .test_07_camera_block import TestCameraBlock
