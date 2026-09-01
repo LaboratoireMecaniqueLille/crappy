71d1e967bd2afc99d33750ba362eaf71e2832d9c
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-09-01 20:19:02 +0200

    tests: add all tests to global test module import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,16 @@
 # coding: utf-8
 
-from . import camera_configuration, camera_configuration_tools, block
+from . import actuator
+from . import block
+from . import blocks
+from . import blocks_gui
+from . import camera
+from . import camera_configuration
+from . import camera_configuration_tools
+from . import camera_process
+from . import camera_processes
+from . import camera_processes_gui
+from . import generator_path
+from . import image_processing
+from . import inout
+from . import modifier
